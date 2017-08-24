421a65ccbd0f87ff9917e112c39b624ac1639369
Tony Zanella <tony.zanella@gmail.com>
2017-08-24 16:49:37 -0700

    Remove unused rails scaffolding (assets, channels, jobs, mailers, views)"

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,9 @@
 require_relative 'boot'
 
-require 'rails/all'
+# Select only parts we need from rails/all
+require 'rails'
+require 'active_record/railtie'
+require 'action_controller/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
