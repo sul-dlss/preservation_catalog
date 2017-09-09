e5233adbca1de4dd04390f1ed60aaf0d6f869953
Johnathan Martin <john.martin@stanford.edu>
2017-09-09 04:41:17 -0700

    preserved_object.preservation_policy now references preservation policy table
    * preservation_policy is now FK to policy table instead of string
      * must be present
    * fix tests

--- app/models/preservation_policy.rb
+++ app/models/preservation_policy.rb
@@ -1,6 +1,7 @@
 ##
 # defines some characteristics about how an object should be preserved, or how an endpoint preserves objects
 class PreservationPolicy < ApplicationRecord
+  has_many :preserved_objects
   has_and_belongs_to_many :endpoints
 
   validates :preservation_policy_name, presence: true
