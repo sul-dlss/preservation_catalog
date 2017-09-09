9abc9369bf8d8ebcb617c879fbda77bade9ff677
Johnathan Martin <john.martin@stanford.edu>
2017-09-09 04:58:56 -0700

    fixup PreservationCopy tests to work with new endpoint cols, including endpoint_type as FK.  test status as FK (instead of string).  fix some prior bad naming.

--- spec/models/preservation_copy_spec.rb
+++ spec/models/preservation_copy_spec.rb
@@ -1,16 +1,30 @@
 require 'rails_helper'
 
 RSpec.describe PreservationCopy, type: :model do
-  let!(:preservation_object2) do
-    PreservedObject.create!(druid: 'ab123cd45679', current_version: 1, preservation_policy: 'keepit', size: 1)
+  let!(:endpoint_type) { EndpointType.create(type_name: 'aws', endpoint_class: 'archive') }
+  let!(:endpoint) do
+    Endpoint.create(
+      endpoint_name: 'aws',
+      endpoint_type_id: endpoint_type.id,
+      endpoint_node: 'sul-sdr',
+      storage_location: '/storage',
+      recovery_cost: '1'
+    )
   end
-  let!(:endpoint2) { Endpoint.create!(endpoint_name: 'oracle', endpoint_type: 'cloud') }
+  let!(:preservation_policy) { PreservationPolicy.create!(preservation_policy_name: 'default') }
+
+  let!(:preserved_object) do
+    PreservedObject.create!(
+      druid: 'ab123cd45679', current_version: 1, preservation_policy_id: preservation_policy.id, size: 1
+    )
+  end
+  let!(:status) { Status.create!(status_text: 'ok') }
   let!(:preservation_copy) do
     PreservationCopy.create!(
-      preserved_object_id: preservation_object2.id,
-      endpoint_id: endpoint2.id,
+      preserved_object_id: preserved_object.id,
+      endpoint_id: endpoint.id,
       current_version: 0,
-      status: 'fixity_error'
+      status_id: status.id
     )
   end
 
@@ -19,7 +33,7 @@ RSpec.describe PreservationCopy, type: :model do
   end
 
   it 'is not valid unless it has all required attributes' do
-    expect(PreservationCopy.new(preserved_object_id: preservation_object2.id)).not_to be_valid
+    expect(PreservationCopy.new(preserved_object_id: preserved_object.id)).not_to be_valid
   end
 
   it 'is valid with valid attributes' do
@@ -28,6 +42,7 @@ RSpec.describe PreservationCopy, type: :model do
 
   it { is_expected.to belong_to(:endpoint) }
   it { is_expected.to belong_to(:preserved_object) }
+  it { is_expected.to belong_to(:status) }
   it { is_expected.to have_db_index(:last_audited) }
   it { is_expected.to have_db_index(:endpoint_id) }
   it { is_expected.to have_db_index(:preserved_object_id) }
