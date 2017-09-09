aa21675effa0eb35b11a7c9b68407269432001de
Johnathan Martin <john.martin@stanford.edu>
2017-09-09 03:31:45 -0700

    add some useful columns to the endpoints table, with appropriate constraints

--- app/models/endpoint.rb
+++ app/models/endpoint.rb
@@ -5,4 +5,7 @@ class Endpoint < ApplicationRecord
   has_many :preservation_copies
   validates :endpoint_name, presence: true, uniqueness: true
   validates :endpoint_type, presence: true
+  validates :endpoint_node, presence: true
+  validates :storage_location, presence: true
+  validates :recovery_cost, presence: true
 end
