51bb564d15e36991cb0f4b20c47fd87f7cd33b7f
Naomi Dushay <ndushay@stanford.edu>
2017-09-14 16:34:50 -0700

    preserved_object.update implemented first pass

--- app/models/preserved_object.rb
+++ app/models/preserved_object.rb
@@ -12,4 +12,38 @@ class PreservedObject < ApplicationRecord
   validates :druid, presence: true, uniqueness: true
   validates :current_version, presence: true
   validates :preservation_policy, null: false
+
+  def self.update(druid, current_version: nil, preservation_policy: nil)
+    # TODO: Add , size: nil,  when we are going to use these variables.
+    existing_rec = find_by(druid: druid)
+    if exists?(druid: druid)
+      # TODO: add more info, e.g. caller, timestamp written to db
+      Rails.logger.debug "update #{druid} called and object exists"
+      if current_version
+        version_comparison = existing_rec.current_version <=> current_version
+        update_entry_per_compare(version_comparison, existing_rec, druid, current_version)
+      end
+      true
+    else
+      create(druid: druid, current_version: current_version, preservation_policy: preservation_policy)
+      Rails.logger.warn "update #{druid} called but object not found; writing object" # TODO: add more info
+      false
+    end
+  end
+
+  private_class_method
+  def self.update_entry_per_compare(version_comparison, existing_rec, druid, current_version)
+    if version_comparison.zero?
+      Rails.logger.info "#{druid} incoming version is equal to db version"
+      existing_rec.touch
+    elsif version_comparison == 1
+      # FIXME: what should happen
+      Rails.logger.warn "#{druid} incoming version smaller than db version"
+      existing_rec.touch
+    elsif version_comparison == -1
+      Rails.logger.info "#{druid} incoming version is greater than db version"
+      existing_rec.current_version = current_version
+      existing_rec.save
+    end
+  end
 end
