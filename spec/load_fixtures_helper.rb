9bc02e879e0800c7982534e682fd592f73641401
Naomi Dushay <ndushay@stanford.edu>
2017-10-03 16:56:32 -0700

    spec/load_fixtures_helper: load moab fixtures into test catalog

---
+++ spec/load_fixtures_helper.rb
@@ -0,0 +1,59 @@
+require 'rails_helper.rb'
+
+RSpec.configure do |rspec|
+  # This config option will be enabled by default on RSpec 4,
+  # but for reasons of backwards compatibility, you have to
+  # set it on RSpec 3.
+  #
+  # It causes the host group and examples to inherit metadata
+  # from the shared context.
+  rspec.shared_context_metadata_behavior = :apply_to_host_groups
+end
+
+RSpec.shared_context "fixture moabs in db" do
+  before(:all) do
+    setup
+    load_fixture_moabs
+  end
+  after(:all) do
+    # TODO: danger - if there are objects in the test db we want to keep
+    Settings.moab.storage_roots.each do |_name, storage_root|
+      storage_dir = File.join(storage_root, Settings.moab.storage_trunk)
+      PreservationCopy.where(endpoint_id: Endpoint.find_by(storage_location: storage_dir).id).each do |pc|
+        po_id = pc.preserved_object_id
+        pc.destroy
+        PreservedObject.find(po_id).destroy
+      end
+    end
+  end
+end
+
+def load_fixture_moabs
+  pp_default = PreservationPolicy.default_preservation_policy
+  status_ok_id = Status.find_by(status_text: 'ok').id
+
+  @moab_storage_dirs.each do |storage_dir|
+    MoabStorageDirectory.find_moab_paths(storage_dir) do |druid, _path, _path_match_data|
+      version = Stanford::StorageServices.current_version(druid)
+      size = Stanford::StorageServices.object_size(druid)
+      po = PreservedObject.create(druid: druid,
+                                  current_version: version,
+                                  size: size,
+                                  preservation_policy: pp_default)
+      PreservationCopy.create(preserved_object_id: po.id,
+                              endpoint_id: @storage_dir_to_endpoint_id[storage_dir],
+                              current_version: version,
+                              status_id: status_ok_id)
+    end
+  end
+end
+
+def setup
+  @moab_storage_dirs = []
+  @storage_dir_to_endpoint_id = {}
+  Settings.moab.storage_roots.each do |_name, storage_root|
+    storage_dir = File.join(storage_root, Settings.moab.storage_trunk)
+    @moab_storage_dirs << storage_dir
+    @storage_dir_to_endpoint_id[storage_dir] = Endpoint.find_by(storage_location: storage_dir).id
+  end
+end
