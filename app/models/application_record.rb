3ce46511459520182e7c008a0ecd451f1df708c2
Christina Harlow <cmharlow@gmail.com>
2017-10-03 16:35:58 -0700

    adding rails instr for activerecord logging

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,3 +1,18 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
+  # Provenance Database Logging Hack
+  $provlogger = Logger.new Rails.root.join('log/test.log')
+
+  if Settings.provlog.enable
+    after_validation :log_errors, :if => Proc.new {|m| m.errors}
+    ActiveSupport::Notifications.subscribe /active_record/ do |*args|
+      event = ActiveSupport::Notifications::Event.new(*args)
+      $provlogger.formatter = Logger::Formatter.new
+      $provlogger.info "#{event.name} : #{event.payload}"
+    end
+  end
+
+  def log_errors
+    $provlogger.info self.errors.full_messages.join(" | ")
+  end
 end
