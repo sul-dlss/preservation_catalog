f26597d9b7fc2392fb4321f93161c16dfa03ce72
Christina Harlow <cmharlow@gmail.com>
2017-10-03 17:08:10 -0700

    fixing rubocop errors & changing log file name to prov

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -1,18 +1,25 @@
 class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
   # Provenance Database Logging Hack
-  $provlogger = Logger.new Rails.root.join('log/test.log')
 
   if Settings.provlog.enable
-    after_validation :log_errors, :if => Proc.new {|m| m.errors}
-    ActiveSupport::Notifications.subscribe /active_record/ do |*args|
+    after_validation :log_errors, if: proc { |m| m.errors }
+    ActiveSupport::Notifications.subscribe(/active_record/) do |*args|
       event = ActiveSupport::Notifications::Event.new(*args)
-      $provlogger.formatter = Logger::Formatter.new
-      $provlogger.info "#{event.name} : #{event.payload}"
+      provlog.formatter = Logger::Formatter.new
+      provlog.info "#{event.name} : #{event.payload}"
     end
   end
 
   def log_errors
-    $provlogger.info self.errors.full_messages.join(" | ")
+    provlog.info errors.full_messages.join(" | ")
+  end
+
+  def self.provlog
+    @provlogger ||= Logger.new Rails.root.join('log', 'prov.log')
+  end
+
+  def provlog
+    self.class.provlog
   end
 end
