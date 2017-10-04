b972ea80a68a5f3305da35174787b0943a2d6dfd
Sarav Shah <saravsshah@gmail.com>
2017-10-04 13:11:15 -0700

    check_online_to_db_existence class method with RSpec tests

---
+++ lib/validations/storage_root_to_db.rb
@@ -0,0 +1,14 @@
+class StorageRootToDB
+  def self.check_online_to_db_existence(storage_dir)
+    results = []
+    MoabStorageDirectory.find_moab_paths(storage_dir) do |druid, _path, _path_match_data|
+      storage_root_current_version = Stanford::StorageServices.current_version(druid)
+      storage_root_size = Stanford::StorageServices.object_size(druid)
+      po_handler = PreservedObjectHandler.new(druid, storage_root_current_version, storage_root_size)
+      results << po_handler.update_or_create
+    end 
+    results
+  end
+end
+
+#two commits Benchmark / RSpec and Method Code
