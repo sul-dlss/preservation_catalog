3d090e6132dd9bd9c7fe45613a71c5d2f7e9fc8b
Sarav Shah <saravsshah@gmail.com>
2017-10-11 12:44:22 -0700

    split PreservedObjectHandler#update_or_create into separate #update and #create methods
    also:
    
    * PreservedObjectHandler result codes
      * a couple new ones
      * some existing ones now take an object type in the %{addl} field
      * slight adjustment of result code to log severity mapping
    * can now use Status.default_status instead of looking up status by 'ok'
    * PreservedObjectHandler now takes storage_dir for moab (for endpoint lookup)
    * supporting modifications to status code and update/create helper methods
    * fix tests
    * add the 'hirb' gem to the development group, for easier db table display

--- lib/benchmarking/moab_to_catalog_testing.rb
+++ lib/benchmarking/moab_to_catalog_testing.rb
@@ -1,13 +1,16 @@
+# bin/rails runner lib/benchmark/moab_to_catalog_testing.rb
+
 require 'benchmark'
 require 'logger'
 require_relative "../audit/moab_to_catalog.rb"
 require 'ruby-prof'
 
 logger = Logger.new('log/benchmark_moab_to_catalog.log')
-at_exit do # File.open "/tmp/MoabtoCatalog_rubyprof_#{Time.now.getlocal}", 'w' do |file|
-  File.open "#{pcc_app_home}/current/log/MoabtoCatalog_rubyprof_#{Time.now.getlocal}", 'w' do |file|
-    RubyProf::FlatPrinter.new(@prof).print(file)
-  end
+at_exit do # #{pcc_app_home}/current/log/MoabtoCatalog_rubyprof_#{Time.now.getlocal}", 'w' do |file|
+
+  # File.open "/tmp/MoabtoCatalog_rubyprof_#{Time.now.getlocal}", 'w' do |file|
+  # RubyProf::FlatPrinter.new(@prof).print(file)
+  # end
   logger.info "Benchmarking stopped at #{Time.now.getlocal}"
 end
 
@@ -26,13 +29,18 @@ else
   logger.info "at revision #{commit}"
 end
 sleep(10)
-# storage_dir = 'spec/fixtures/moab_storage_root'
-storage_dir = '/services-disk12/sdr2objects'
-time_to_check_existence = Benchmark.realtime do
-  RubyProf.start
-  logger.info "Check for output"
-  StorageRootToDB.check_moab_to_catalog_existence(storage_dir)
-  @prof = RubyProf.stop
+# storage_dir = 'spec/fixtures/storage_root01/moab_storage_trunk'
+# storage_dir = '/services-disk12/sdr2objects'
+# this is coming from config/settings.yml and config/initializers
+Stanford::StorageServices.storage_roots.each do |storage_root|
+  time_to_check_existence = Benchmark.realtime do
+    RubyProf.start
+    logger.info "Check for output"
+
+    MoabToCatalog.check_moab_to_catalog_existence(File.join(storage_root, Moab::Config.storage_trunk))
+    # when creating add true parameter, and if updating leave blank
+    # @prof = RubyProf.stop
+  end
 end
 logger.info time_to_check_existence.to_s
 logger.info "It took #{time_to_check_existence} seconds to check the database w/ online moab"
