fb18e237d8444898e8de73a04232c9b6ecb7b1ba
Sarav Shah <saravsshah@gmail.com>
2017-10-12 15:30:48 -0700

    fix benchmark

--- lib/benchmarking/moab_to_catalog_testing.rb
+++ lib/benchmarking/moab_to_catalog_testing.rb
@@ -8,9 +8,9 @@ require 'ruby-prof'
 
 logger = Logger.new('log/benchmark_moab_to_catalog.log')
 at_exit do # #{pcc_app_home}/current/log/MoabtoCatalog_rubyprof_#{Time.now.getlocal}", 'w' do |file|
-  File.open "/tmp/MoabtoCatalog_rubyprof_#{Time.now.getlocal}", 'w' do |file|
-    RubyProf::FlatPrinter.new(@prof).print(file)
-  end
+  # File.open "/tmp/MoabtoCatalog_rubyprof_#{Time.now.getlocal}", 'w' do |file|
+    # RubyProf::FlatPrinter.new(@prof).print(file)
+  # end
   logger.info "Benchmarking stopped at #{Time.now.getlocal}"
 end
 
@@ -33,11 +33,11 @@ sleep(10)
 # storage_dir = '/services-disk12/sdr2objects'
 Stanford::StorageServices.storage_roots.each do |storage_root|
   time_to_check_existence = Benchmark.realtime do
-    RubyProf.start
+    # RubyProf.start
     logger.info "Check for output"
-    StorageRootToDB.check_moab_to_catalog_existence(File.join(storage_root,Moab::Config.storage_trunk)#when creating add true parameter, and if updating leave blank
-    @prof = RubyProf.stop
+    MoabToCatalog.check_moab_to_catalog_existence(File.join(storage_root,Moab::Config.storage_trunk)) #when creating add true parameter, and if updating leave blank
+    # @prof = RubyProf.stop
   end
+  logger.info time_to_check_existence.to_s
+  logger.info "It took #{time_to_check_existence} seconds to check the database w/ online moab"
 end
-logger.info time_to_check_existence.to_s
-logger.info "It took #{time_to_check_existence} seconds to check the database w/ online moab"
