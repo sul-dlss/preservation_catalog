4ca087c6fe69a51c38310657677accb14b909f98
Naomi Dushay <ndushay@stanford.edu>
2017-10-12 18:28:48 -0700

    models: specify dependent: :raise_with_exception for has_many relation

--- app/models/preserved_object.rb
+++ app/models/preserved_object.rb
@@ -5,7 +5,7 @@
 # those instances.
 class PreservedObject < ApplicationRecord
   belongs_to :preservation_policy
-  has_many :preservation_copies
+  has_many :preservation_copies, dependent: :restrict_with_exception
   validates :druid, presence: true, uniqueness: true, format: { with: /\A[a-z]{2}\d{3}[a-z]{2}\d{4}\z/ }
   validates :current_version, presence: true, numericality: { only_integer: true, greater_than: 0 }
   # NOTE: size here is approximate and not used for fixity checking
