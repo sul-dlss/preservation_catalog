bb5f354d5cca56ac50951babbd7646353714e165
Tony Zanella <tony.zanella@gmail.com>
2017-10-13 16:59:29 -0700

    cap links to config/settings to pick up shared configs

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,7 +24,7 @@ before 'deploy:restart', 'shared_configs:update'
 append :linked_files, "config/database.yml" # , "config/secrets.yml"
 
 # Default value for linked_dirs is []
-append :linked_dirs, "log" # , "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
+append :linked_dirs, "log", "config/settings" # , "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
