f81f5a3071d8f746db6d76e01751df9a828a6e76
Naomi Dushay <ndushay@stanford.edu>
2017-10-20 17:03:06 -0700

    moab_to_catalog.seed_catalog added

--- lib/audit/moab_to_catalog.rb
+++ lib/audit/moab_to_catalog.rb
@@ -1,6 +1,9 @@
 ##
-# Method that will check the a single moab service disk for the existence of moabs in postgres database
+# finds Moab objects on a single Moab storage_dir and interacts with Catalog (db)
+#   according to method called
 class MoabToCatalog
+
+  # NOTE: shameless green! code duplication with seed_catalog
   def self.check_existence(storage_dir, expect_to_create=false)
     results = []
     MoabStorageDirectory.find_moab_paths(storage_dir) do |druid, path, _path_match_data|
@@ -10,6 +13,21 @@ class MoabToCatalog
         results << po_handler.update
       else
         Rails.logger.error "druid: #{druid} expected to exist in catalog but was not found" unless expect_to_create
+        results << po_handler.create
+      end
+    end
+    results
+  end
+
+  # NOTE: shameless green! code duplication with check_existence
+  def self.seed_catalog(storage_dir)
+    results = []
+    MoabStorageDirectory.find_moab_paths(storage_dir) do |druid, path, _path_match_data|
+      moab = Moab::StorageObject.new(druid, path)
+      po_handler = PreservedObjectHandler.new(druid, moab.current_version_id, moab.size, storage_dir)
+      if PreservedObject.exists?(druid: druid)
+        Rails.logger.error "druid: #{druid} NOT expected to exist in catalog but was found"
+      else
         results << po_handler.create
       end
     end
