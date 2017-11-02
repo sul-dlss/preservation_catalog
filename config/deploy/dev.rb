bc905a9807c2e44f90ecad6d77a7943276c283c7
Tony Zanella <tony.zanella@gmail.com>
2017-11-02 11:02:58 -0700

    Remove cap stage for sul-pcc-dev: the machine is gone
      Fixes #243

--- config/deploy/dev.rb
+++
@@ -1,5 +0,0 @@
-server 'sul-pcc-dev.stanford.edu', user: 'pres', roles: %w[app db web]
-
-Capistrano::OneTimeKey.generate_one_time_key!
-set :rails_env, 'development'
-set :bundle_without, 'deploy test'
