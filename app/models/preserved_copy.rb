9f538259e02904f2d21ce5a4fe07c5ab26fa5fdc
Sarav Shah <saravsshah@gmail.com>
2017-12-04 11:57:34 -0800

    add enum validations and tests

--- app/models/preserved_copy.rb
+++ app/models/preserved_copy.rb
@@ -3,21 +3,20 @@
 class PreservedCopy < ApplicationRecord
   belongs_to :preserved_object
   belongs_to :endpoint
-  belongs_to :status
 
   validates :preserved_object, presence: true
   validates :endpoint, presence: true
   validates :version, presence: true
-  validates :status, presence: true
   # NOTE: size here is approximate and not used for fixity checking
   validates :size, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
   enum status: {
     ok: 0,
     invalid_moab: 1,
     invalid_checksum: 2,
-    unexpected_version: 3,
-    not_found_on_disk: 4,
-    expected_version_not_found_on_disk: 5,
-    fixity_check_failed: 6
+    online_moab_not_found: 3,
+    expected_version_not_found_online: 4,
+    fixity_check_failed: 5
   }
+  DEFAULT_STATUS = statuses[:ok].freeze
+  validates :status, inclusion: { in: statuses.keys }
 end
--- spec/models/preserved_copy_spec.rb
+++ spec/models/preserved_copy_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe PreservedCopy, type: :model do
     policy_id = PreservationPolicy.default_preservation_policy.id
     PreservedObject.create!(druid: 'ab123cd4567', current_version: 1, preservation_policy_id: policy_id)
   end
-  let!(:status) { Status.default_status }
+  let!(:status) { described_class.statuses[:ok] }
   let!(:preserved_copy) do
     PreservedCopy.create!(
       preserved_object_id: preserved_object.id,
@@ -29,9 +29,31 @@ RSpec.describe PreservedCopy, type: :model do
     expect(preserved_copy).to be_valid
   end
 
+  it 'defines a status enum with the expected values' do
+    is_expected.to define_enum_for(:status).with(
+      ok: 0,
+      invalid_moab: 1,
+      invalid_checksum: 2,
+      online_moab_not_found: 3,
+      expected_version_not_found_online: 4,
+      fixity_check_failed: 5
+    )
+  end
+
+  it 'is not valid without an existing status' do
+    expect {
+      PreservedCopy.new(
+        preserved_object_id: preserved_object.id,
+        endpoint_id: endpoint.id,
+        version: 0,
+        status: 6,
+        size: 1
+      )
+    }.to raise_error(ArgumentError, "'6' is not a valid status")
+  end
+
   it { is_expected.to belong_to(:endpoint) }
   it { is_expected.to belong_to(:preserved_object) }
-  it { is_expected.to belong_to(:status) }
   it { is_expected.to have_db_index(:last_audited) }
   it { is_expected.to have_db_index(:endpoint_id) }
   it { is_expected.to have_db_index(:preserved_object_id) }
