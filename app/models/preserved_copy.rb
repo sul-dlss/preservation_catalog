73771fd35374dac7bf8cc18c130718ae1b3302d0
Johnathan Martin <john.martin@stanford.edu>
2017-12-05 13:40:19 -0800

    a bit of re-arrangement so that like things are grouped contiguously

--- app/models/preserved_copy.rb
+++ app/models/preserved_copy.rb
@@ -1,15 +1,6 @@
 ##
 # PreservedCopy represents a concrete instance of a PreservedObject, in physical storage on some node.
 class PreservedCopy < ApplicationRecord
-  belongs_to :preserved_object
-  belongs_to :endpoint
-
-  validates :preserved_object, presence: true
-  validates :endpoint, presence: true
-  validates :version, presence: true
-
-  # NOTE: size here is approximate and not used for fixity checking
-  validates :size, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
   enum status: {
     ok: 0,
     invalid_moab: 1,
@@ -18,6 +9,16 @@ class PreservedCopy < ApplicationRecord
     expected_version_not_found_online: 4,
     fixity_check_failed: 5
   }
+
   DEFAULT_STATUS = statuses[:ok].freeze
+
+  belongs_to :preserved_object
+  belongs_to :endpoint
+
+  validates :endpoint, presence: true
+  validates :preserved_object, presence: true
+  # NOTE: size here is approximate and not used for fixity checking
+  validates :size, numericality: { only_integer: true, greater_than: 0 }, allow_nil: true
   validates :status, inclusion: { in: statuses.keys }
+  validates :version, presence: true
 end
