482fb45f5925d7f662e1fd2f315b20662f30703a
Tony Zanella <tony.zanella@gmail.com>
2017-12-20 16:21:47 -0800

    Add webmock and spec for wf_errors_reporter
      Change workflow url in settings
      to only matter for prod

---
+++ spec/services/workflow_errors_reporter_spec.rb
@@ -0,0 +1,22 @@
+require 'rails_helper'
+
+RSpec.describe WorkflowErrorsReporter do
+  describe '.update_workflow(druid, error_message)' do
+    let(:body) { "<process name='moab-valid' status='error' errorMessage=' Invalid moab, validation errors: [\"Should contain only sequential version directories. Current directories: [\\\"v0001\\\", \\\"v0002\\\", \\\"v0009\\\"]\"]'/>" }
+    let(:error_msg) do
+      [{ 10 => "PreservedObjectHandler(jj925bx9565, 9, 6570668, <Endpoint:...>) PreservedObject db object does not exist" },
+       { 13 => "PreservedObjectHandler(jj925bx9565, 9, 6570668, <Endpoint:...>) Invalid moab, validation errors: [\"Should contain only sequential version directories. Current directories: [\\\"v0001\\\", \\\"v0002\\\", \\\"v0009\\\"]\"]" }]
+    end
+
+    before do
+      Settings.workflow_services.url = 'https://sul-lyberservices-test.stanford.edu/workflow/'
+      stub_request(:put, 'https://sul-lyberservices-test.stanford.edu/workflow/dor/objects/druid:jj925bx9565/workflows/preservationWF/moab-valid')
+        .with(body: body,
+              headers: { 'Content-Type' => 'application/xml' })
+        .to_return(status: 204, body: "", headers: {})
+    end
+    it 'updates preservationWF with invalid moab errors' do
+      expect(described_class.update_workflow('jj925bx9565', error_msg)).to eq [error_msg[1]]
+    end
+  end
+end
