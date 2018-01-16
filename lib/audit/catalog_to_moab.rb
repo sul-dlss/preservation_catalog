2eb75ec668ad7fe856bba2c497de1345811ca3a9
Naomi Dushay <ndushay@stanford.edu>
2018-01-16 15:18:19 -0800

    lib/audit/catalog_to_moab: add all methods needed for rake tasks

--- lib/audit/catalog_to_moab.rb
+++ lib/audit/catalog_to_moab.rb
@@ -1,32 +1,62 @@
 require 'druid-tools'
+require 'profiler.rb'
 
 # Catalog to Moab existence check code
 class CatalogToMoab
 
-  # NOTE: this is at the chicken scratches stage
-  #  I'm not convinced we'll _ever_ do the whole db this way;  it was a way to start
-  #    step 1: ensure load_fixtures_helper works with very rough code here
-  # FIXME:  temporarily turning off rubocop
+  # FIXME:  temporarily turning off rubocop until we migrate the code to its final home
   # rubocop:disable all
-  def self.check_existence_on_dir
+  def self.check_version_on_dir(last_checked_b4_date, storage_dir)
+    # TODO: ensure last_checked_version_b4_date is in the right format
+
+    # FIXME:  move the guts of this to someplace else (possibly leaving SQL query here, but object manipulation elsewhere)
+    #sql = '' # get all PC with last_version_audit date < threshold_date
+    #  build a params hash and send it to find_each  in order to chunk it
+
+    #  we'll NEVER do the whole db this way;  it was a way to start --
+    #   replace with appropriate chunked sql query ...
     PreservedCopy.find_each do |pc|
+      # TODO: probably move of this to a method in PreservedCopy object, or pohandler or?
       id = pc.preserved_object.druid
-      version = pc.current_version
+      catalog_version = pc.current_version
       storage_location = pc.endpoint.storage_location
       druid = DruidTools::Druid.new(id)
       object_dir = "#{storage_location}/#{druid.tree.join('/')}"
 
       moab = Moab::StorageObject.new(id, object_dir)
       moab_version = moab.current_version_id
-      if version == moab_version
-        p "hurray - #{id} versions match: #{version}"
+      if catalog_version == moab_version
+        p "hurray - #{id} versions match: #{catalog_version}"
       else
-        p "boo - #{id} catalog has #{version} but moab has #{moab_version}"
+        p "boo - #{id} catalog has #{catalog_version} but moab has #{moab_version}"
       end
 
-      # TODO: update status, timestamps, report errors, log, etc.
+      # TODO: update status, timestamps, report errors, log, etc. -- methods in PreservedCopy model? pohandler?
     end
   end
   # rubocop:enable all
 
+  def self.check_version_on_dir_profiled(last_checked_b4_date, storage_dir)
+    profiler = Profiler.new
+    profiler.prof { check_version_on_dir(last_checked_b4_date, storage_dir) }
+    profiler.print_results_flat('C2M_check_version_on_dir')
+  end
+
+  def self.check_version_all_dirs(last_checked_b4_date)
+    Settings.moab.storage_roots.each do |strg_root_name, strg_root_location|
+      start_msg = "#{Time.now.utc.iso8601} C2M check_version starting for '#{strg_root_name}' at #{strg_root_location}"
+      puts start_msg
+      Rails.logger.info start_msg
+      check_version_on_dir(last_checked_b4_date, "#{strg_root_location}/#{Settings.moab.storage_trunk}")
+      end_msg = "#{Time.now.utc.iso8601} C2M check_version ended for '#{strg_root_name}' at #{strg_root_location}"
+      puts end_msg
+      Rails.logger.info end_msg
+    end
+  end
+
+  def self.check_version_all_dirs_profiled(last_checked_b4_date)
+    profiler = Profiler.new
+    profiler.prof { check_version_all_dirs(last_checked_b4_date) }
+    profiler.print_results_flat('C2M_check_version_all_dirs')
+  end
 end
--- spec/lib/audit/catalog_to_moab_spec.rb
+++ spec/lib/audit/catalog_to_moab_spec.rb
@@ -3,11 +3,61 @@ require_relative '../../load_fixtures_helper.rb'
 
 RSpec.describe CatalogToMoab do
   include_context 'fixture moabs in db'
-  describe '.check_existence_on_dir' do
-    # NOTE: this is at the chicken scratches stage
-    #  step 1: ensure load_fixtures_helper works
-    it 'test_of_load_fixtures_helper (eventually will be testing code for .check_existence_on_dir)' do
-      described_class.check_existence_on_dir
+  let(:last_checked_version_b4_date) { (Time.now.utc - 1.day).iso8601 }
+  let(:storage_dir) { 'spec/fixtures/storage_root01/moab_storage_trunk' }
+
+  context '.check_version_on_dir' do
+    let(:subject) { described_class.check_version_on_dir(last_checked_version_b4_date, storage_dir) }
+
+    # NOTE: this test will be removed once we test other things
+    it 'test_of_load_fixtures_helper (eventually will be testing code for .check_version_on_dir)' do
+      subject
+    end
+    it 'does something' do
+      skip('TODO: implement method (probably elsewhere) and write test saying we call the method?')
+    end
+  end
+
+  context ".check_version_on_dir_profiled" do
+    let(:subject) { described_class.check_version_on_dir_profiled(last_checked_version_b4_date, storage_dir) }
+
+    it "spins up a profiler, calling profiling and printing methods on it" do
+      mock_profiler = instance_double(Profiler)
+      expect(Profiler).to receive(:new).and_return(mock_profiler)
+      expect(mock_profiler).to receive(:prof)
+      expect(mock_profiler).to receive(:print_results_flat).with('C2M_check_version_on_dir')
+      subject
+    end
+  end
+
+  context '.check_version_all_dirs' do
+    let(:subject) { described_class.check_version_all_dirs(last_checked_version_b4_date) }
+
+    it 'calls .check_version_for_dir once per storage root' do
+      expect(described_class).to receive(:check_version_on_dir).exactly(Settings.moab.storage_roots.count).times
+      subject
+    end
+
+    it 'calls check_version_for_dir with the right arguments' do
+      Settings.moab.storage_roots.each do |storage_root|
+        expect(described_class).to receive(:check_version_on_dir).with(
+          last_checked_version_b4_date,
+          "#{storage_root[1]}/#{Settings.moab.storage_trunk}"
+        )
+      end
+      subject
+    end
+  end
+
+  context ".check_version_all_dirs_profiled" do
+    let(:subject) { described_class.check_version_all_dirs_profiled(last_checked_version_b4_date) }
+
+    it "spins up a profiler, calling profiling and printing methods on it" do
+      mock_profiler = instance_double(Profiler)
+      expect(Profiler).to receive(:new).and_return(mock_profiler)
+      expect(mock_profiler).to receive(:prof)
+      expect(mock_profiler).to receive(:print_results_flat).with('C2M_check_version_all_dirs')
+      subject
     end
   end
 end
