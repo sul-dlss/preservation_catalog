540f71274f0e6bf30592a5b70d7b2e81bf2fe61a
Naomi Dushay <ndushay@stanford.edu>
2018-01-22 11:39:17 -0800

    catalog_to_moab: add github issue numbers to TODO comments

--- lib/audit/catalog_to_moab.rb
+++ lib/audit/catalog_to_moab.rb
@@ -6,6 +6,7 @@ class CatalogToMoab
 
   # allows for sharding/parallelization by storage_dir
   def self.check_version_on_dir(last_checked_b4_date, storage_dir)
+    # TODO: ensure last_checked_version_b4_date is in the right format for query - see #485
     pcs = PreservedCopy
           .where('last_version_audit < ? OR last_version_audit IS NULL', last_checked_b4_date)
           .order('last_version_audit IS NOT NULL, last_version_audit ASC')
@@ -43,7 +44,7 @@ class CatalogToMoab
   # FIXME:  temporarily turning off rubocop until we migrate the code to its final home
   # rubocop:disable all
   private_class_method def self.check_catalog_version(preserved_copy, storage_dir)
-    # TODO: Pohandler.ensure_po_version_matches_this_pc_version (for non-archived, online moab)
+    # TODO: Pohandler.ensure_po_version_matches_this_pc_version (for non-archived, online moab) - see #483
 
     id = preserved_copy.preserved_object.druid
     catalog_version = preserved_copy.version
@@ -52,21 +53,22 @@ class CatalogToMoab
     object_dir = "#{storage_location}/#{DruidTools::Druid.new(id).tree.join('/')}"
 
     moab = Moab::StorageObject.new(id, object_dir)
-    # TODO: report error if moab doesn't exist
+    # TODO: report error if moab doesn't exist - see #482
 
     moab_version = moab.current_version_id
 
-    # TODO: anything special if preserved_copy.status is not OK_STATUS?
+    # TODO: anything special if preserved_copy.status is not OK_STATUS? - see #480
 
     if catalog_version == moab_version
       p "hooray - #{id} versions match: #{catalog_version}"
       results.add_result(PreservedObjectHandlerResults::VERSION_MATCHES, preserved_copy.class.name)
-      # TODO:  original spec asks for verifying files????  read audit requirements
+      # TODO:  original spec asks for verifying files????  read audit requirements - see #481
       results.report_results
     elsif catalog_version < moab_version
       results.add_result(PreservedObjectHandlerResults::UNEXPECTED_VERSION, preserved_copy.class.name)
       p "boo - #{id} catalog has #{catalog_version} but moab has #{moab_version}"
       # update the catalog
+      # TODO: avoid repetitious results ... (leave out line above??) - see #484
       pohandler = PreservedObjectHandler.new(id, moab_version, moab.size, preserved_copy.endpoint)
       pohandler.update_version_after_validation # results reported by this call
     else # catalog_version > moab_version
@@ -81,8 +83,8 @@ class CatalogToMoab
     end
 
     # TODO: call these methods on PreservedCopy object
-    # update_pc_audit_timestamps(preserved_copy, ran_moab_validation, true)
-    # update_db_object(preserved_copy)
+    # update_pc_audit_timestamps(preserved_copy, ran_moab_validation, true) - see #477
+    # update_db_object(preserved_copy) - see #478
   end
   # rubocop:enable all
 
