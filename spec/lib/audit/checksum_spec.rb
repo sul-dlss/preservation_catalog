a3e2541c160df97ca3a0657eab0247fdb032c7a2
Tony Zanella <tony.zanella@gmail.com>
2018-02-12 16:32:31 -0800

    Reduce duplication in lib/audit/checksum class
      Fixes #572

--- spec/lib/audit/checksum_spec.rb
+++ spec/lib/audit/checksum_spec.rb
@@ -6,13 +6,13 @@ require_relative "../../../lib/audit/checksum.rb"
 
 RSpec.describe Checksum do
 
-  describe ".checksum_validate_disk" do
-    described_class.checksum_validate_disk('2018-02-05 21:37:23 UTC', 'services-disk04', 'MD5')
+  describe ".validate_disk" do
+    described_class.validate_disk('2018-02-05 21:37:23 UTC', 'services-disk04', 'MD5')
     skip 'we should figure out what they are and test them'
   end
 
-  describe ".checksum_validate_disk_profiled" do
-    let(:subject) { described_class.checksum_validate_disk_profiled(Time.now.utc, 'services-disk04', 'MD5') }
+  describe ".validate_disk_profiled" do
+    let(:subject) { described_class.validate_disk_profiled(Time.now.utc, 'services-disk04', 'MD5') }
 
     it "spins up a profiler, calling profiling and printing methods on it" do
       mock_profiler = instance_double(Profiler)
@@ -22,26 +22,26 @@ RSpec.describe Checksum do
       subject
     end
 
-    it "calls .checksum_validate_disk" do
-      expect(described_class).to receive(:checksum_validate_disk)
+    it "calls .validate_disk" do
+      expect(described_class).to receive(:validate_disk)
       subject
     end
   end
 
-  describe ".checksum_validate_disk_all_endpoints" do
-    it 'calls checksum_validate_disk once per endpoint' do
-      described_class.checksum_validate_disk_all_endpoints('2018-02-05 21:37:23 UTC', 'MD5')
+  describe ".validate_disk_all_endpoints" do
+    it 'calls validate_disk once per endpoint' do
+      described_class.validate_disk_all_endpoints('2018-02-05 21:37:23 UTC', 'MD5')
       skip 'we should figure out what they are and test them'
     end
 
-    it 'calls checksum_validate_disk with the right arguments' do
-      described_class.checksum_validate_disk_all_endpoints('2018-02-05 21:37:23 UTC', 'MD5')
+    it 'calls validate_disk with the right arguments' do
+      described_class.validate_disk_all_endpoints('2018-02-05 21:37:23 UTC', 'MD5')
       skip 'we should figure out what they are and test them'
     end
   end
 
-  describe ".checksum_validate_disk_all_endpoints_profiled" do
-    let(:subject) { described_class.checksum_validate_disk_all_endpoints_profiled(Time.now.utc, 'MD5') }
+  describe ".validate_disk_all_endpoints_profiled" do
+    let(:subject) { described_class.validate_disk_all_endpoints_profiled(Time.now.utc, 'MD5') }
 
     it "spins up a profiler, calling profiling and printing methods on it" do
       mock_profiler = instance_double(Profiler)
@@ -50,8 +50,8 @@ RSpec.describe Checksum do
       expect(mock_profiler).to receive(:print_results_flat).with('CV_checksum_validation_all_endpoints')
       subject
     end
-    it "calls .checksum_validate_disk_all_endpoints" do
-      expect(described_class).to receive(:checksum_validate_disk_all_endpoints)
+    it "calls .validate_disk_all_endpoints" do
+      expect(described_class).to receive(:validate_disk_all_endpoints)
       subject
     end
   end
