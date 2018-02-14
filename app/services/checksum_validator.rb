23afc9dc6534da8a7015c38c6c0ccbe0581d1be1
Tony Zanella <tony.zanella@gmail.com>
2018-02-13 17:04:30 -0800

    Explore a solution for #521
      One way to compare checksums for each manifestInventory
      of a moab, and one way to validate fixity for each entry
      in the lates version of the signature catalog

--- app/services/checksum_validator.rb
+++ app/services/checksum_validator.rb
@@ -9,4 +9,41 @@ class ChecksumValidator
   def validate_checksum
     # TODO: implement this;  we begin with a placeholder
   end
+
+  # exploring a solution for #521 --
+  # for each manInv file, compare checksums
+  def validate_manifest_inventories
+    results = []
+    storage_object.version_list.each do |storage_object_version|
+      results << storage_object_version.verify_manifest_inventory
+    end
+    results.map(&:verified)
+  end
+
+  # exploring a solution for #522
+  # for latest sigcat, compare checksums
+  def validate_signature_catalog
+    results = []
+    signature_catalog_entries.each do |sig_cat_entry|
+      calculated_signature = Moab::FileSignature.new.signature_from_file(sig_cat_entry_path(sig_cat_entry))
+      results << sig_cat_entry.signature.eql?(calculated_signature)
+    end
+    results
+  end
+
+  def storage_object
+    Moab::StorageObject.new(@druid, druid_path)
+  end
+
+  def signature_catalog_entries
+    storage_object.version_list.last.signature_catalog.entries
+  end
+
+  def druid_path
+    "#{@endpoint.storage_location}/#{DruidTools::Druid.new(@druid).tree.join('/')}"
+  end
+
+  def sig_cat_entry_path(e)
+    Pathname("#{druid_path}/#{Moab::StorageObject.version_dirname(e.version_id)}/data/#{e.group_id}/#{e.path}")
+  end
 end
