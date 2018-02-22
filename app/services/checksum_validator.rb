493008b4b409a9077fbd667ef6dc23877d71f0f1
Sarav Shah <saravsshah@gmail.com>
2018-02-21 17:30:55 -0800

    verify manifestInvetory.xml checksums with error handling

--- app/services/checksum_validator.rb
+++ app/services/checksum_validator.rb
@@ -1,23 +1,87 @@
 # code for validating Moab checksums
 class ChecksumValidator
-  def initialize(druid, endpoint, algorithm)
+  
+  attr_reader :handler_results
+
+  MANIFESTS = 'manifests'.freeze
+  MANIFESTS_XML = 'manifestInventory.xml'.freeze
+
+
+  def initialize(druid, endpoint)
     @druid = druid
     @endpoint = endpoint
-    @algorithm = algorithm
+    @handler_results = AuditResults.new(druid, nil, endpoint)
   end
 
   def validate_checksum
     # TODO: implement this;  we begin with a placeholder
   end
 
-  # exploring a solution for #521 --
-  # for each manInv file, compare checksums
   def validate_manifest_inventories
     results = []
     storage_object.version_list.each do |storage_object_version|
-      results << storage_object_version.verify_manifest_inventory
+      validate_manifest_inventory(storage_object_version)
     end
-    results.map(&:verified)
+    handler_results.report_results
+  end
+
+  private
+
+  def validate_manifest_inventory(storage_object_version)
+    manifest_file_path = "#{storage_object_version.version_pathname}/#{MANIFESTS}/#{MANIFESTS_XML}"
+    begin
+      verification_result = storage_object_version.verify_manifest_inventory
+      parse_verification_subentities(verification_result)
+    rescue Nokogiri::XML::SyntaxError => e
+      handler_results.add_result(AuditResults::INVALID_MANIFEST, manifest_file_path: manifest_file_path)
+    rescue Errno::ENOENT => e
+      handler_results.add_result(AuditResults::MANIFEST_NOT_IN_MOAB, manifest_file_path: manifest_file_path )
+    end
+  end
+
+  def parse_verification_subentities(result)
+    return if result.verified
+    result.subentities.each do |subentity|
+      parse_verification_subentity(subentity) unless subentity.verified
+    end
+  end
+
+  def parse_verification_subentity(subentity)
+    xml_modified(subentity) unless subentity.details.dig('group_differences', 'manifests', 'subsets', 'modified').nil?
+
+    xml_added(subentity) unless subentity.details.dig('group_differences', 'manifests', 'subsets', 'added').nil?
+
+    xml_deleted(subentity) unless subentity.details.dig('group_differences', 'manifests', 'subsets', 'deleted').nil?
+  end
+
+  def xml_modified(subentity)
+    subentity.details.dig('group_differences', 'manifests', 'subsets', 'modified', 'files').each_value do |details|
+      mismatch_error_message = {
+        file_path: "#{subentity.details['other']}/#{details['basis_path']}",
+        version: subentity.details['basis']
+      }
+      handler_results.add_result(AuditResults::MOAB_FILE_CHECKSUM_MISMATCH, mismatch_error_message)
+    end
+  end
+
+  def xml_added(subentity)
+    subentity.details.dig('group_differences', 'manifests', 'subsets', 'added', 'files').each_value do |details|
+      absent_from_manifest_message = {
+        file_path: "#{subentity.details['other']}/#{details['other_path']}",
+        manifest_file_path: "#{subentity.details['other']}/manifestInventory.xml"
+      }
+      handler_results.add_result(AuditResults::FILE_NOT_IN_MANIFEST, absent_from_manifest_message)
+    end
+  end
+
+  def xml_deleted(subentity)
+    subentity.details.dig('group_differences', 'manifests', 'subsets', 'deleted', 'files').each_value do |details|
+      absent_from_moab_message = {
+        manifest_file_path: "#{subentity.details['other']}/manifestInventory.xml",
+        file_path: "#{subentity.details['other']}/#{details['basis_path']}"
+      }
+      handler_results.add_result(AuditResults::FILE_NOT_IN_MOAB, absent_from_moab_message)
+    end
   end
 
   # exploring a solution for #522
