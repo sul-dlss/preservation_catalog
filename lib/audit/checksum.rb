1919e775c761c02d829845aa2bc7e9569cc88698
Sarav Shah <saravsshah@gmail.com>
2018-02-26 11:56:26 -0800

    Change method parameters and names

--- lib/audit/checksum.rb
+++ lib/audit/checksum.rb
@@ -8,22 +8,22 @@ class Checksum
   # rubocop:disable Style/EmptyMethod:
   # TODO: implement this;  we begin with a placeholder
 
-  def self.validate_disk(last_checked_b4, endpoint, algorithm="MD5")
+  def self.validate_disk(last_checked_b4, storage_dir)
   end
 
-  def self.validate_disk_profiled(last_checked_b4, endpoint, algorithm)
+  def self.validate_disk_profiled(last_checked_b4, storage_dir)
     profiler = Profiler.new
-    profiler.prof { validate_disk(last_checked_b4, endpoint, algorithm) }
+    profiler.prof { validate_disk(last_checked_b4, storage_dir) }
     profiler.print_results_flat('CV_checksum_validation_on_dir')
   end
 
-  def self.validate_disk_all_endpoints(last_checked_b4, algorithm="md5")
+  def self.validate_disk_all_dirs(last_checked_b4)
   end
 
-  def self.validate_disk_all_endpoints_profiled(last_checked_b4, algorithm)
+  def self.validate_disk_all_dirs_profiled(last_checked_b4)
     profiler = Profiler.new
-    profiler.prof { validate_disk_all_endpoints(last_checked_b4, algorithm) }
-    profiler.print_results_flat('CV_checksum_validation_all_endpoints')
+    profiler.prof { validate_disk_all_dirs(last_checked_b4) }
+    profiler.print_results_flat('CV_checksum_validation_all_dirs')
   end
 
 end
--- spec/lib/audit/checksum_spec.rb
+++ spec/lib/audit/checksum_spec.rb
@@ -7,12 +7,12 @@ require_relative "../../../lib/audit/checksum.rb"
 RSpec.describe Checksum do
 
   describe ".validate_disk" do
-    described_class.validate_disk('2018-02-05 21:37:23 UTC', 'services-disk04', 'MD5')
+    described_class.validate_disk('2018-02-05 21:37:23 UTC', "spec/fixtures/storage_root01/moab_storage_trunk")
     skip 'we should figure out what they are and test them'
   end
 
   describe ".validate_disk_profiled" do
-    let(:subject) { described_class.validate_disk_profiled(Time.now.utc, 'services-disk04', 'MD5') }
+    let(:subject) { described_class.validate_disk_profiled(Time.now.utc,  "spec/fixtures/storage_root01/moab_storage_trunk") }
 
     it "spins up a profiler, calling profiling and printing methods on it" do
       mock_profiler = instance_double(Profiler)
@@ -28,30 +28,30 @@ RSpec.describe Checksum do
     end
   end
 
-  describe ".validate_disk_all_endpoints" do
-    it 'calls validate_disk once per endpoint' do
-      described_class.validate_disk_all_endpoints('2018-02-05 21:37:23 UTC', 'MD5')
+  describe ".validate_disk_all_dirs" do
+    it 'calls validate_disk once per storage root' do
+      described_class.validate_disk_all_dirs('2018-02-05 21:37:23 UTC')
       skip 'we should figure out what they are and test them'
     end
 
     it 'calls validate_disk with the right arguments' do
-      described_class.validate_disk_all_endpoints('2018-02-05 21:37:23 UTC', 'MD5')
+      described_class.validate_disk_all_dirs('2018-02-05 21:37:23 UTC')
       skip 'we should figure out what they are and test them'
     end
   end
 
-  describe ".validate_disk_all_endpoints_profiled" do
-    let(:subject) { described_class.validate_disk_all_endpoints_profiled(Time.now.utc, 'MD5') }
+  describe ".validate_disk_all_dirs_profiled" do
+    let(:subject) { described_class.validate_disk_all_dirs_profiled(Time.now.utc) }
 
     it "spins up a profiler, calling profiling and printing methods on it" do
       mock_profiler = instance_double(Profiler)
       expect(Profiler).to receive(:new).and_return(mock_profiler)
       expect(mock_profiler).to receive(:prof)
-      expect(mock_profiler).to receive(:print_results_flat).with('CV_checksum_validation_all_endpoints')
+      expect(mock_profiler).to receive(:print_results_flat).with('CV_checksum_validation_all_dirs')
       subject
     end
-    it "calls .validate_disk_all_endpoints" do
-      expect(described_class).to receive(:validate_disk_all_endpoints)
+    it "calls .validate_disk_all_dirs" do
+      expect(described_class).to receive(:validate_disk_all_dirs)
       subject
     end
   end
