5ad846800cc97e0ad9dc38811becd590e7f354de
Sarav Shah <saravsshah@gmail.com>
2018-03-15 16:04:09 -0700

    RSpec tests for C2M.check_version_druid

--- spec/lib/audit/catalog_to_moab_class_spec.rb
+++ spec/lib/audit/catalog_to_moab_class_spec.rb
@@ -89,4 +89,46 @@ RSpec.describe CatalogToMoab do
       subject
     end
   end
+
+  context '.check_version_druid' do
+    include_context 'fixture moabs in db'
+    let(:druid) { 'bj102hs9687' }
+    let(:subject) { described_class.check_version_druid(last_checked_version_b4_date, druid, limit) }
+
+    context 'when there are PreservedCopies to check' do
+      let(:c2m_mock) { instance_double(described_class) }
+
+      it 'creates an instance and calls #check_catalog_version for every result when results are in a single batch' do
+        allow(described_class).to receive(:new).and_return(c2m_mock)
+        expect(c2m_mock).to receive(:check_catalog_version).exactly(1).times
+        described_class.check_version_druid(last_checked_version_b4_date, druid, limit)
+      end
+
+      it 'creates an instance and calls #check_catalog_version on everything in batches' do
+        # note that there are 3 objects to be processed, and we are setting batch limit to 2,
+        # and we are ensuring that we call #check_catalog_version on all 3 objects.
+
+        # we must set up all the described_class instance objects ahead of any process calling CatalogToMoab.new
+        pcs_from_scope =
+          PreservedCopy.least_recent_version_audit(last_checked_version_b4_date).by_druid(druid)
+        c2m_list = pcs_from_scope.map do |pc|
+          described_class.new(pc, storage_dir)
+        end
+        c2m_list.each do |c2m|
+          allow(described_class).to receive(:new).with(c2m.preserved_copy, storage_dir).and_return(c2m)
+          expect(c2m).to receive(:check_catalog_version).exactly(1).times.and_call_original
+        end
+        described_class.check_version_druid(last_checked_version_b4_date, druid, 2)
+      end
+    end
+
+    context 'when there are no PreservedCopies to check' do
+      it 'will not create an instance to call check_catalog_version on' do
+        allow(described_class).to receive(:new)
+        PreservedCopy.all.update(last_version_audit: (Time.now.utc + 2.days))
+        expect(described_class).not_to receive(:new)
+        subject
+      end
+    end
+  end
 end
