853d7bf10d2f8924b32a10f39d20a5b751ba0460
Sarav Shah <saravsshah@gmail.com>
2018-04-05 14:11:40 -0700

    update version routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :catalog, only: [:create]
+  resources :catalog, param: :druid, only: %i[create update]
   resources :moab_storage, only: %i[index show]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
