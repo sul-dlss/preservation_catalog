8b33b15adb5d6c29c794454a14709e252f03ae7d
Tony Zanella <tony.zanella@gmail.com>
2018-04-06 12:11:50 -0700

    Deploy to new prod vm

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -1,4 +1,5 @@
 server 'preservation-catalog-prod.stanford.edu', user: 'pres', roles: %w[app db web]
+server 'preservation-catalog-prod-a.stanford.edu', user: 'pres', roles: %w[app db web]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
