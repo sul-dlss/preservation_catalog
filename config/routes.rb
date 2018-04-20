4fa64aae72ef07719fa31c385615c01af4908765
Tony Zanella <tony.zanella@gmail.com>
2018-04-20 11:49:06 -0700

    Set up resque dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
+require 'resque/server'
+
 Rails.application.routes.draw do
   resources :catalog, param: :druid, only: %i[create update]
   resources :moab_storage, only: %i[index show]
+
+  mount Resque::Server.new, at: "/resque"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
