b04e425e89a520a1c8ea8c267f25a2b7e4c700f1
Tony Zanella <tony.zanella@gmail.com>
2018-04-23 14:24:12 -0700

    Add worker machine to capistrano stage stage

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,7 +1,10 @@
 server 'preservation-catalog-stage.stanford.edu', user: 'pres', roles: %w[app db web]
+server 'preservation-worker-stage.stanford.edu', user: 'pres', roles: %w[app db web]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
 set :bundle_without, 'deploy test'
 set :deploy_to, '/opt/app/pres/preservation_catalog'
-append :linked_files, "config/newrelic.yml"
+
+# uncomment if/when we turn newrelic on
+# append :linked_files, "config/newrelic.yml"
