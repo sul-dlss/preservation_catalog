4ff73be5d478330172bdc259d711000341c11f2e
Joe Atzberger <ohiocore@gmail.com>
2018-04-23 11:58:00 -0700

    Configure resque as the job adapter in prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,8 +53,8 @@ Rails.application.configure do
   # config.cache_store = :mem_cache_store
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
-  # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "preservation_catalog_#{Rails.env}"
+  config.active_job.queue_adapter     = :resque
+  config.active_job.queue_name_prefix = "preservation_catalog_#{Rails.env}"
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
