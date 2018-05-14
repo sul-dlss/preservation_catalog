655c1adbd9aa1af73fdd83197f145ca708b9aada
Joe Atzberger <ohiocore@gmail.com>
2018-05-14 16:52:33 -0700

    Allow individuals settings to be overridden via (bash) ENV
    This follows what we did w/ Hyku and greatly flexibility.
    See: https://github.com/railsconfig/config#fine-tuning
    
    Example usage, overriding `Settings.zip_storage` for a single process
    without changing the YAML file or code:
    ```
    SETTINGS__ZIP_STORAGE=foobar be rails c
    [1] pry(main)> Settings.zip_storage
    => "foobar"
    ```

--- config/initializers/config.rb
+++ config/initializers/config.rb
@@ -1,4 +1,7 @@
 Config.setup do |config|
   # Name of the constant exposing loaded settings
   config.const_name = 'Settings'
+  config.env_prefix = 'SETTINGS'
+  config.env_separator = '__'
+  config.use_env = true
 end
