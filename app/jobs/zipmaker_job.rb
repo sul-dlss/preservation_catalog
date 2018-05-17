d65c1e560c10f23bd21a995bfb4f45038c17c732
Tony Zanella <tony.zanella@gmail.com>
2018-05-16 17:20:47 -0700

    Add code, fixture, spec for zipmaker worker
      Fixes #707

--- app/jobs/zipmaker_job.rb
+++ app/jobs/zipmaker_job.rb
@@ -1,3 +1,4 @@
+require 'open3'
 # Responsibilities:
 # locate files
 # zip files
@@ -9,6 +10,26 @@ class ZipmakerJob < ApplicationJob
   # @param [String] druid
   # @param [Integer] version
   def perform(druid, version)
+    binary_path = Moab::StorageServices.object_version_path(druid, version)
+    zip_path = DruidVersionZip.new(druid, version).file_path
+    unless File.exist?(zip_path)
+      ZipmakerJob.zip_binary(zip_path, binary_path) if binary_path
+    end
     PlexerJob.perform_later(druid, version)
+  end
+
+  # @param [String] path to druid version zip
+  # @param [String] path to druid version
+  # @todo calculate md5 of zip for plexer
+  def self.zip_binary(zip_path, binary_path)
+    _output, error, status = Open3.capture3(zip_command(zip_path, binary_path))
+    raise "zipmaker failure #{error}" unless status.success?
+  end
+
+  # @param [String] path to druid version zip
+  # @param [String] path to druid version
+  # @return [String]
+  def self.zip_command(zip_path, binary_path)
+    "zip -vr0X -s 10g #{zip_path} #{binary_path}"
   end
 end
--- spec/jobs/zipmaker_job_spec.rb
+++ spec/jobs/zipmaker_job_spec.rb
@@ -3,13 +3,77 @@ require 'rails_helper'
 describe ZipmakerJob, type: :job do
   let(:druid) { 'bj102hs9687' }
   let(:version) { 1 }
+  let(:binary_path) { Moab::StorageServices.object_version_path(druid, version) }
+  let(:zip_path) { "spec/fixtures/transfers/bj/102/hs/9687/#{druid}#{format('.v%04d.zip', version)}" }
+
+  before do
+    allow(PlexerJob).to receive(:perform_later).with(any_args)
+    allow(Settings).to receive(:zip_storage).and_return('spec/fixtures/transfers')
+  end
 
   it 'descends from ApplicationJob' do
     expect(described_class.new).to be_an(ApplicationJob)
   end
 
-  it 'invokes PlexerJob' do
-    expect(PlexerJob).to receive(:perform_later).with(druid, version)
-    described_class.perform_now(druid, version)
+  context 'zip already exists in zip storage' do
+
+    it 'does nothing' do
+      expect(File).to exist(zip_path)
+      expect(described_class).not_to receive(:zip_binary)
+      described_class.perform_now(druid, version)
+    end
+  end
+
+  context 'zip is not yet in zip storage' do
+    let(:version) { 2 }
+
+    after { File.delete(zip_path) }
+
+    it 'zips up the druid version into zip storage' do
+      described_class.perform_now(druid, version)
+      expect(File).to exist(zip_path)
+    end
+
+    it 'invokes PlexerJob' do
+      expect(PlexerJob).to receive(:perform_later).with(druid, version)
+      described_class.perform_now(druid, version)
+    end
+  end
+
+  describe '.zip_binary' do
+    let(:version) { 2 }
+
+    context 'succeeds in zipping the binary' do
+      it 'does not raise an error' do
+        expect { described_class.zip_binary(zip_path, binary_path) }.not_to raise_error
+        File.delete(zip_path)
+      end
+    end
+
+    context 'fails to zip the binary' do
+      context 'when inpath is incorrect' do
+        it 'raises error' do
+          expect { described_class.zip_binary(zip_path, 'bar') }.to raise_error(RuntimeError, /zipmaker failure/)
+        end
+      end
+      context 'when options are unsupported' do
+        it 'raises error' do
+          allow(described_class).to receive(:zip_command).and_return("zip -a #{zip_path} #{binary_path}")
+          expect { described_class.zip_binary(zip_path, binary_path) }.to raise_error(RuntimeError, /zipmaker failure/)
+        end
+      end
+      context 'if the utility "moved"' do
+        it 'raises error' do
+          allow(described_class).to receive(:zip_command).and_return("zap -vr0X -s 10g #{zip_path} #{binary_path}")
+          expect { described_class.zip_binary(zip_path, binary_path) }.to raise_error(Errno::ENOENT, /No such file/)
+        end
+      end
+    end
+
+    describe '.zip_command' do
+      it 'returns a string representing the command to zip' do
+        expect(described_class.zip_command(zip_path, binary_path)).to eq "zip -vr0X -s 10g #{zip_path} #{binary_path}"
+      end
+    end
   end
 end
