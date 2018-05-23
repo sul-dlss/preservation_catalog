21a07dc30a344c0a9e8e65abccea51342b32ef48
Naomi Dushay <ndushay@stanford.edu>
2018-05-23 15:33:01 -0700

    deploy:  only seed for db role

--- config/deploy.rb
+++ config/deploy.rb
@@ -42,7 +42,7 @@ after 'deploy:migrate', 'db_seed'
 
 desc 'Run rake db:seed'
 task :db_seed do
-  on roles(:all) do
+  on roles(:db) do
     within current_path do
       with rails_env: fetch(:rails_env) do
         execute :rake, 'db:seed'
