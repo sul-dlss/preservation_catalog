0b5649af87d0533df8b9253fee1a3af37f90c6b3
Naomi Dushay <ndushay@stanford.edu>
2018-06-02 11:00:56 -0700

    schedule: m2c, c2m only on correct Sats, and not silenced

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,22 +1,24 @@
 # Use this file to easily define all of your cron jobs.
 # Learn more: http://github.com/javan/whenever
 
-# run these task only on servers with the correct roles in Capistrano
-# these tasks append to existing logs
-every '0 5 1-7 * 6', roles: [:m2c] do
+# also overriding b/c whenever adds --silent (https://github.com/javan/whenever/issues/524, #541)
+job_type :sat_only_rake, "cd :path && bin/is_it_saturday.sh && :environment_variable=:environment :bundle_command rake :task :output"
+
+# these append to existing logs
+every '0 5 1-7 * *', roles: [:m2c] do
   set :output, standard: 'log/m2c.log', error: 'log/m2c-err.log'
-  rake 'm2c_exist_all_storage_roots'
+  sat_only_rake 'm2c_exist_all_storage_roots'
 end
-every '0 5 15-21 * 6', roles: [:m2c] do
+every '0 5 15-21 * *', roles: [:m2c] do
   set :output, standard: 'log/m2c.log', error: 'log/m2c-err.log'
-  rake 'm2c_exist_all_storage_roots'
+  sat_only_rake 'm2c_exist_all_storage_roots'
 end
 
-every '0 5 8-14 * 6', roles: [:c2m] do
+every '0 5 8-14 * *', roles: [:c2m] do
   set :output, standard: 'log/c2m.log', error: 'log/c2m-err.log'
-  rake "c2m_check_version_all_dirs[`date --date='7 days ago' --iso-8601=s`]"
+  sat_only_rake "c2m_check_version_all_dirs[`date --date='7 days ago' --iso-8601=s`]"
 end
-every '0 5 22-28 * 6', roles: [:c2m] do
+every '0 5 22-28 * *', roles: [:c2m] do
   set :output, standard: 'log/c2m.log', error: 'log/c2m-err.log'
-  rake "c2m_check_version_all_dirs[`date --date='7 days ago' --iso-8601=s`]"
+  sat_only_rake "c2m_check_version_all_dirs[`date --date='7 days ago' --iso-8601=s`]"
 end
