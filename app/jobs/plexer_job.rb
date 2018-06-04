949e237ac89f4564f3e0dd85bf7a755d3461753e
Joe Atzberger <ohiocore@gmail.com>
2018-06-04 16:53:41 -0700

    Plexer adds ZipChecksum rows and updates PC#size info

--- app/jobs/plexer_job.rb
+++ app/jobs/plexer_job.rb
@@ -5,7 +5,7 @@
 #   Endpoint1Delivery.perform_later(druid, version)
 #   Endpoint2Delivery.perform_later(druid, version)
 #   ...
-class PlexerJob < ApplicationJob
+class PlexerJob < DruidVersionJobBase
   queue_as :zips_made
 
   before_enqueue { |job| job.zip_info_check!(job.arguments.third) }
@@ -14,26 +14,44 @@ class PlexerJob < ApplicationJob
   # @param [Integer] version
   # @param [Hash<Symbol => String>] metadata Zip info
   # @option metadata [Integer] :size
-  # @option metadata [String] :zip_cmd
   # @option metadata [String] :checksum_md5
+  # @option metadata [String] :zip_cmd
+  # @option metadata [String] :zip_version
   def perform(druid, version, metadata)
-    targets(druid, version).each do |worker|
-      worker.perform_later(druid, version, metadata)
+    ApplicationRecord.transaction do
+      update_size(metadata[:size])
+      pcs.each do |pc|
+        pc.zip_checksums.find_or_create_by!(
+          md5: metadata[:checksum_md5],
+          create_info: metadata.slice(:zip_cmd, :zip_version).to_s
+        )
+      end
     end
+    targets(pcs.pluck(:endpoint_id)).each { |worker| worker.perform_later(druid, version, metadata) }
   end
 
+  # @return [PreservedCopy::ActiveRecord_Relation]
+  def pcs
+    @pcs ||= PreservedCopy.by_druid(zip.druid.id).for_archive_endpoints.where(version: zip.version)
+  end
+
+  # @param [Array<Integer>] endpoint_ids
   # @return [Array<Class>] Endpoint delivery classes to be targeted
-  def targets(druid, version)
+  def targets(endpoint_ids)
     Endpoint
-      .joins(:endpoint_type, preserved_copies: [:preserved_object])
-      .where(
-        endpoint_types: { endpoint_class: 'archive' },
-        preserved_objects: { druid: druid },
-        preserved_copies: { version: version }
-      )
-      .map do |ep|
-        Rails.logger.error("Archive Endpoint (id: #{ep.id}) has no delivery_class") unless ep.delivery_class
-        ep.delivery_class
+      .where(id: endpoint_ids)
+      .group(:delivery_class, :id)
+      .pluck(:delivery_class, :id)
+      .map do |pair|
+        Rails.logger.error("Archive Endpoint (id: #{pair.second}) has no delivery_class") unless pair.first
+        pair.first
       end.compact
+  end
+
+  # Validate the incoming value and save once for all
+  # @param [Integer] size
+  def update_size(size)
+    raise "Inavlid size #{size}" unless size.is_a?(Integer) && size > 0
+    pcs.update_all(size: size) # rubocop:disable Rails/SkipsModelValidations
   end
 end
--- spec/jobs/plexer_job_spec.rb
+++ spec/jobs/plexer_job_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 describe PlexerJob, type: :job do
-  let(:job) { described_class.new }
+  let(:job) { described_class.new(druid, version, metadata).tap { |j| j.zip = DruidVersionZip.new(druid, version) } }
   let(:druid) { 'bj102hs9687' }
+  let(:endpoint) { create(:archive_endpoint, delivery_class: 1) } # default
   let(:version) { 1 }
-  let(:md5) { '1B2M2Y8AsgTpgAmY7PhCfg==' }
+  let(:md5) { 'd41d8cd98f00b204e9800998ecf8427e' }
   let(:metadata) do
     {
       checksum_md5: md5,
@@ -13,15 +14,27 @@ describe PlexerJob, type: :job do
       zip_version: 'Zip 3.0 (July 5th 2008)'
     }
   end
+  let(:po) { create(:preserved_object, druid: druid, current_version: version) }
 
-  it 'descends from ApplicationJob' do
-    expect(job).to be_an(ApplicationJob)
+  before { allow(S3EndpointDeliveryJob).to receive(:perform_later).with(any_args) }
+
+  it 'descends from DruidVersionJobBase' do
+    expect(job).to be_an(DruidVersionJobBase)
+  end
+
+  it 'raises without enqueueing if metadata is incomplete' do
+    expect { described_class.perform_later(druid, version, metadata.merge(size: nil)) }
+      .to raise_error(ArgumentError, /size/)
+    expect { described_class.perform_later(druid, version, metadata.reject { |x| x == :zip_cmd }) }
+      .to raise_error(ArgumentError, /zip_cmd/)
   end
 
   describe '#perform' do
-    before { allow(job).to receive(:targets).and_return([S3EndpointDeliveryJob]) }
+    let!(:pc) { create(:archive_copy, preserved_object: po, version: version) }
+    let(:zc) { pc.zip_checksums.first }
 
     it 'splits the message out to endpoint(s)' do
+      allow(job).to receive(:targets).and_return([S3EndpointDeliveryJob])
       expect(S3EndpointDeliveryJob).to receive(:perform_later)
         .with(
           druid,
@@ -30,28 +43,46 @@ describe PlexerJob, type: :job do
         )
       job.perform(druid, version, metadata)
     end
+
+    it 'adds zip_checksum rows' do
+      allow(job).to receive(:targets).and_return([])
+      job.perform(druid, version, metadata)
+      expect(pc).not_to be_nil
+      expect(zc.md5).to eq md5
+      expect(zc.create_info).to eq metadata.slice(:zip_cmd, :zip_version).to_s
+    end
+
+    it 'updates PreservedCopy#size' do
+      job.perform(druid, version, metadata)
+      expect(pc.reload.size).to eq metadata[:size]
+    end
   end
 
   describe '#targets' do
-    let(:po) { create(:preserved_object, druid: druid) }
+    let(:endpoint_ids) { po.preserved_copies.pluck(:endpoint_id) }
 
     before { create(:preserved_copy, preserved_object: po, version: version, endpoint: endpoint) }
 
-    context 'with undeliverable PC Endpoints' do
+    it 'returns classes' do
+      expect(Rails.logger).not_to receive(:error)
+      expect(job.targets(endpoint_ids)).to eq [S3EndpointDeliveryJob]
+    end
+
+    context 'with undeliverable PC Endpoint(s)' do
       let(:endpoint) { create(:archive_endpoint, delivery_class: nil) }
 
       it 'logs but does not raise' do
         expect(Rails.logger).to receive(:error).with(/no delivery_class/)
-        expect(job.targets(druid, version)).to eq []
+        expect(job.targets(endpoint_ids)).to eq []
       end
     end
 
-    context 'with deliverable PC Endpoints' do
-      let(:endpoint) { create(:archive_endpoint, delivery_class: 1) }
+    context 'with non-PC Endpoint(s)' do
+      let(:endpoint) { create(:endpoint) }
 
-      it 'returns classes' do
-        expect(Rails.logger).not_to receive(:error)
-        expect(job.targets(druid, version)).to eq [S3EndpointDeliveryJob]
+      it 'returns empty Array' do
+        expect(Rails.logger).to receive(:error).with(/no delivery_class/)
+        expect(job.targets(endpoint_ids)).to eq []
       end
     end
   end
