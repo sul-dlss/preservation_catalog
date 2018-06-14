612ef0181e5b1843a2d4107d4e6c712854ef37c7
Johnathan Martin <john.martin@stanford.edu>
2018-06-14 09:59:31 -0700

    line in between constants and first method, make string use in update_workflow_status call consistent w/ other strings in the class

--- app/services/workflow_reporter.rb
+++ app/services/workflow_reporter.rb
@@ -5,6 +5,8 @@ class WorkflowReporter
   DOR = 'dor'.freeze
   PRESERVATIONAUDITWF = 'preservationAuditWF'.freeze
   NO_WORKFLOW_HOOKUP = 'no workflow hookup - assume you are in test or dev environment'.freeze
+  COMPLETED = 'completed'.freeze
+
   # this method will always return true because of the dor-workflow-service gem
   # see issue sul-dlss/dor-workflow-service#50 for more context
   def self.report_error(druid, process_name, error_message)
@@ -17,7 +19,7 @@ class WorkflowReporter
 
   def self.report_completed(druid, process_name)
     if Settings.workflow_services_url.present?
-      Dor::WorkflowService.update_workflow_status(DOR, "druid:#{druid}", PRESERVATIONAUDITWF, process_name, 'completed')
+      Dor::WorkflowService.update_workflow_status(DOR, "druid:#{druid}", PRESERVATIONAUDITWF, process_name, COMPLETED)
     else
       Rails.logger.warn(NO_WORKFLOW_HOOKUP)
     end
