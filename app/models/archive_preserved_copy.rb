419f77bb0e236eac7fa1350d6f4a6eed0aa8c993
Johnathan Martin <john.martin@stanford.edu>
2018-07-17 12:13:12 -0700

    grammar pedantry

--- app/models/archive_preserved_copy.rb
+++ app/models/archive_preserved_copy.rb
@@ -1,4 +1,4 @@
-# Corresponds to a Moab-Version on an ZipEndpoint.
+# Corresponds to a Moab-Version on a ZipEndpoint.
 #   There will be individual parts (at least one) - see ArchivepreservedCopyPart.
 # For a fully consistent system, given an (Online) PreservedCopy, the number of associated
 # ArchivePreservedCopy objects should be:
