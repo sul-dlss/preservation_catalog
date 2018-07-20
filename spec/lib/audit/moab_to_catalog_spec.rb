c970ac365f9c47e220003f3e2677310a54e75c44
Joe Atzberger <ohiocore@gmail.com>
2018-07-20 15:08:56 -0700

    Refactor moab_to_catalog_spec.rb
    - `let(:subject)` is an antipattern, especially when the **timing** of
      the invocation of `subject` is critical.  Just make the actual call.
    - pull out common `let` statements
    - use `.change` blocks

--- spec/lib/audit/moab_to_catalog_spec.rb
+++ spec/lib/audit/moab_to_catalog_spec.rb
@@ -4,6 +4,16 @@ require 'stringio'
 RSpec.describe Audit::MoabToCatalog do
   let(:storage_dir) { 'spec/fixtures/storage_root01/sdr2objects' }
   let(:endpoint) { Endpoint.find_by!(storage_location: storage_dir) }
+  let(:mock_profiler) do
+    prof = instance_double(Profiler, prof: nil)
+    allow(Profiler).to receive(:new).and_return(prof)
+    prof
+  end
+  let(:moab) do
+    m = instance_double(Moab::StorageObject, object_pathname: storage_dir, :storage_root= => nil)
+    allow(Moab::StorageObject).to receive(:new).and_return(m)
+    m
+  end
 
   before do
     PreservationPolicy.seed_from_config
@@ -23,94 +33,60 @@ RSpec.describe Audit::MoabToCatalog do
   end
 
   describe ".check_existence_for_all_storage_roots" do
-    let(:subject) { described_class.check_existence_for_all_storage_roots }
-
     it 'calls check_existence_for_dir once per storage root' do
       expect(described_class).to receive(:check_existence_for_dir).exactly(HostSettings.storage_roots.entries.count).times
-      subject
+      described_class.check_existence_for_all_storage_roots
     end
 
     it 'calls check_existence_for_dir with the right arguments' do
       HostSettings.storage_roots.to_h.each_value do |path|
         expect(described_class).to receive(:check_existence_for_dir).with("#{path}/#{Settings.moab.storage_trunk}")
       end
-      subject
+      described_class.check_existence_for_all_storage_roots
     end
   end
 
   describe ".check_existence_for_all_storage_roots_profiled" do
-    let(:subject) { described_class.check_existence_for_all_storage_roots_profiled }
-
     it "spins up a profiler, calling profiling and printing methods on it" do
-      mock_profiler = instance_double(Profiler)
-      expect(Profiler).to receive(:new).and_return(mock_profiler)
       expect(mock_profiler).to receive(:prof)
       expect(mock_profiler).to receive(:print_results_flat).with('M2C_check_existence_for_all_storage_roots')
-
-      subject
+      described_class.check_existence_for_all_storage_roots_profiled
     end
   end
 
   describe ".seed_catalog_for_all_storage_roots" do
-    let(:subject) { described_class.seed_catalog_for_all_storage_roots }
-
     it 'calls seed_catalog_for_dir once per storage root' do
       expect(described_class).to receive(:seed_catalog_for_dir).exactly(HostSettings.storage_roots.entries.count).times
-      subject
+      described_class.seed_catalog_for_all_storage_roots
     end
 
     it 'calls seed_catalog_for_dir with the right arguments' do
       HostSettings.storage_roots.to_h.each_value do |path|
         expect(described_class).to receive(:seed_catalog_for_dir).with("#{path}/#{Settings.moab.storage_trunk}")
       end
-      subject
+      described_class.seed_catalog_for_all_storage_roots
     end
   end
 
   describe ".seed_catalog_for_all_storage_roots_profiled" do
-    let(:subject) { described_class.seed_catalog_for_all_storage_roots_profiled }
-
     it "spins up a profiler, calling profiling and printing methods on it" do
-      mock_profiler = instance_double(Profiler)
-
-      expect(Profiler).to receive(:new).and_return(mock_profiler)
       expect(mock_profiler).to receive(:prof)
       expect(mock_profiler).to receive(:print_results_flat).with('seed_catalog_for_all_storage_roots')
-
-      subject
+      described_class.seed_catalog_for_all_storage_roots_profiled
     end
   end
 
   describe ".check_existence_for_dir" do
-    let(:subject) { described_class.check_existence_for_dir(storage_dir) }
-
     it "calls 'find_moab_paths' with appropriate argument" do
       expect(Stanford::MoabStorageDirectory).to receive(:find_moab_paths).with(storage_dir)
-      subject
+      described_class.check_existence_for_dir(storage_dir)
     end
 
-    it 'gets moab current version from Moab::StorageObject' do
-      moab = instance_double(Moab::StorageObject)
-      allow(moab).to receive(:storage_root=)
-      allow(moab).to receive(:object_pathname).and_return(storage_dir)
-      allow(moab).to receive(:size)
+    it 'gets moab size and current version from Moab::StorageObject' do
       expect(moab).to receive(:current_version_id).at_least(1).times
-      allow(Moab::StorageObject).to receive(:new).and_return(moab)
-
-      expect(Moab::StorageServices).not_to receive(:new)
-      subject
-    end
-
-    it 'gets moab size from Moab::StorageObject' do
-      moab = instance_double(Moab::StorageObject)
-      allow(moab).to receive(:storage_root=)
-      allow(moab).to receive(:object_pathname).and_return(storage_dir)
-      allow(moab).to receive(:current_version_id)
       expect(moab).to receive(:size).at_least(1).times
-      allow(Moab::StorageObject).to receive(:new).and_return(moab)
-
       expect(Moab::StorageServices).not_to receive(:new)
-      subject
+      described_class.check_existence_for_dir(storage_dir)
     end
 
     context "(calls check_existence)" do
@@ -141,12 +117,12 @@ RSpec.describe Audit::MoabToCatalog do
           expect(arg_hash[:po_handler]).to receive(:check_existence).and_return(fake_codes)
         end
         # * 3 will magically give us a flat, 6 element array
-        expect(subject).to eq(fake_codes * 3)
+        expect(described_class.check_existence_for_dir(storage_dir)).to eq(fake_codes * 3)
       end
     end
 
     it "return correct number of results" do
-      expect(subject.count).to eq 6
+      expect(described_class.check_existence_for_dir(storage_dir).count).to eq 6
     end
     it "storage directory doesn't exist (misspelling, read write permissions)" do
       allow(Endpoint).to receive(:find_by!).and_return(instance_double(Endpoint))
@@ -155,28 +131,22 @@ RSpec.describe Audit::MoabToCatalog do
       )
     end
     it "storage directory exists but it is empty" do
-      storage_dir = 'spec/fixtures/empty/sdr2objects'
-      expect(described_class.check_existence_for_dir(storage_dir)).to eq []
+      expect(described_class.check_existence_for_dir('spec/fixtures/empty/sdr2objects')).to eq []
     end
   end
 
   describe ".check_existence_for_dir_profiled" do
     let(:storage_dir) { "spec/fixtures/storage_root01/sdr2objects" }
-    let(:subject) { described_class.check_existence_for_dir_profiled(storage_dir) }
 
     it "spins up a profiler, calling profiling and printing methods on it" do
-      mock_profiler = instance_double(Profiler)
-      expect(Profiler).to receive(:new).and_return(mock_profiler)
       expect(mock_profiler).to receive(:prof)
       expect(mock_profiler).to receive(:print_results_flat).with('M2C_check_existence_for_dir')
-
-      subject
+      described_class.check_existence_for_dir_profiled(storage_dir)
     end
   end
 
   describe '.check_existence_for_druid' do
     let(:druid) { 'bz514sm9647' }
-    let(:subject) { described_class.check_existence_for_druid(druid) }
     let(:results) do
       [{ db_obj_does_not_exist: "PreservedObject db object does not exist" },
        { created_new_object: "added object to db as it did not exist" }]
@@ -184,11 +154,11 @@ RSpec.describe Audit::MoabToCatalog do
 
     it 'finds the relevant moab' do
       expect(Stanford::StorageServices).to receive(:find_storage_object).with(druid).and_call_original
-      subject
+      described_class.check_existence_for_druid(druid)
     end
     it 'finds the correct Endpoint' do
       expect(Endpoint).to receive(:find_by!).with(storage_location: storage_dir)
-      subject
+      described_class.check_existence_for_druid(druid)
     end
     it 'calls pohandler.check_existence' do
       po_handler = instance_double('PreservedObjectHandler')
@@ -200,10 +170,10 @@ RSpec.describe Audit::MoabToCatalog do
       ).and_return(po_handler)
       expect(po_handler).to receive(:logger=)
       expect(po_handler).to receive(:check_existence)
-      subject
+      described_class.check_existence_for_druid(druid)
     end
     it 'returns results' do
-      expect(subject).to eq results
+      expect(described_class.check_existence_for_druid(druid)).to eq results
     end
   end
 
@@ -218,35 +188,16 @@ RSpec.describe Audit::MoabToCatalog do
   end
 
   describe ".seed_catalog_for_dir" do
-    let(:subject) { described_class.seed_catalog_for_dir(storage_dir) }
-
     it "calls 'find_moab_paths' with appropriate argument" do
       expect(Stanford::MoabStorageDirectory).to receive(:find_moab_paths).with(storage_dir)
-      subject
+      described_class.seed_catalog_for_dir(storage_dir)
     end
 
-    it 'gets moab current version from Moab::StorageObject' do
-      moab = instance_double(Moab::StorageObject)
-      allow(moab).to receive(:storage_root=)
-      allow(moab).to receive(:object_pathname).and_return(storage_dir)
-      allow(moab).to receive(:size)
-      expect(moab).to receive(:current_version_id).at_least(1).times
-      allow(Moab::StorageObject).to receive(:new).and_return(moab)
-
-      expect(Moab::StorageServices).not_to receive(:new)
-      subject
-    end
-
-    it 'gets moab size from Moab::StorageObject' do
-      moab = instance_double(Moab::StorageObject)
-      allow(moab).to receive(:storage_root=)
-      allow(moab).to receive(:object_pathname).and_return(storage_dir)
-      allow(moab).to receive(:current_version_id)
+    it 'gets moab size and current version from Moab::StorageObject' do
       expect(moab).to receive(:size).at_least(1).times
-      allow(Moab::StorageObject).to receive(:new).and_return(moab)
-
+      expect(moab).to receive(:current_version_id).at_least(1).times
       expect(Moab::StorageServices).not_to receive(:new)
-      subject
+      described_class.seed_catalog_for_dir(storage_dir)
     end
 
     context "(creates after validation)" do
@@ -274,12 +225,12 @@ RSpec.describe Audit::MoabToCatalog do
         expected_argument_list.each do |arg_hash|
           expect(arg_hash[:po_handler]).to receive(:create_after_validation)
         end
-        subject
+        described_class.seed_catalog_for_dir(storage_dir)
       end
     end
 
     it "return correct number of results" do
-      expect(subject.count).to eq 3
+      expect(described_class.seed_catalog_for_dir(storage_dir).count).to eq 3
     end
     it "storage directory doesn't exist (misspelling, read write permissions)" do
       allow(Endpoint).to receive(:find_by!).and_return(instance_double(Endpoint))
@@ -288,28 +239,19 @@ RSpec.describe Audit::MoabToCatalog do
       )
     end
     it "storage directory exists but it is empty" do
-      storage_dir = 'spec/fixtures/empty/sdr2objects'
-      expect(described_class.check_existence_for_dir(storage_dir)).to eq []
+      expect(described_class.check_existence_for_dir('spec/fixtures/empty/sdr2objects')).to eq []
     end
   end
 
   describe ".drop_endpoint" do
-    let(:subject) { described_class.drop_endpoint('fixture_sr1') }
-
-    before do
-      described_class.seed_catalog_for_all_storage_roots
-    end
+    before { described_class.seed_catalog_for_all_storage_roots }
 
     it 'drops PreservedCopies that correspond to the given endpoint' do
-      expect(PreservedCopy.count).to eq 16
-      subject
-      expect(PreservedCopy.count).to eq 13
+      expect { described_class.drop_endpoint('fixture_sr1') }.to change { PreservedCopy.count }.from(16).to(13)
     end
 
     it 'drops PreservedObjects that correspond to the given endpoint' do
-      expect(PreservedObject.count).to eq 16
-      subject
-      expect(PreservedObject.count).to eq 13
+      expect { described_class.drop_endpoint('fixture_sr1') }.to change { PreservedObject.count }.from(16).to(13)
     end
 
     it 'rolls back pres obj delete if pres copy cannot be deleted' do
@@ -318,52 +260,33 @@ RSpec.describe Audit::MoabToCatalog do
       allow(PreservedObject).to receive(:left_outer_joins).with(:preserved_copies).and_return(active_record_double1)
       allow(active_record_double1).to receive(:where).with(preserved_copies: { id: nil }).and_return(active_record_double2)
       allow(active_record_double2).to receive(:destroy_all).and_raise(ActiveRecord::ActiveRecordError, 'foo')
-      begin
-        subject
-      rescue
-        # Expect this to fail and don't need error handling in the .drop_endpoint class method
-        # let subject still run instead of catching ActiveRecordError and stop the execution
-      end
+      expect { described_class.drop_endpoint('fixture_sr1') }.to raise_error(ActiveRecord::ActiveRecordError)
       expect(PreservedCopy.count).to eq 16
       expect(PreservedObject.count).to eq 16
     end
   end
 
   describe ".populate_endpoint" do
-    let(:endpoint_name) { 'fixture_sr1' }
-    let(:subject) { described_class.populate_endpoint(endpoint_name) }
-
-    before do
-      described_class.seed_catalog_for_all_storage_roots
-    end
+    before { described_class.seed_catalog_for_all_storage_roots }
 
     it "won't change objects in a fully seeded db" do
-      subject
-      expect(PreservedCopy.count).to eq 16
+      expect { described_class.populate_endpoint('fixture_sr1') }.not_to change { PreservedCopy.count }.from(16)
       expect(PreservedObject.count).to eq 16
     end
 
     it 're-adds objects for a dropped endpoint' do
-      described_class.drop_endpoint(endpoint_name)
-      expect(PreservedCopy.count).to eq 13
+      described_class.drop_endpoint('fixture_sr1')
       expect(PreservedObject.count).to eq 13
-      subject
-      expect(PreservedCopy.count).to eq 16
+      expect { described_class.populate_endpoint('fixture_sr1') }.to change { PreservedCopy.count }.from(13).to(16)
       expect(PreservedObject.count).to eq 16
     end
   end
 
-  describe ".populate_endpoint.profiled" do
-    let(:root) { 'fixture_sr1' }
-    let(:subject) { described_class.populate_endpoint_profiled(root) }
-
+  describe ".populate_endpoint_profiled" do
     it "spins up a profiler, calling profiling and printing methods on it" do
-      mock_profiler = instance_double(Profiler)
-      expect(Profiler).to receive(:new).and_return(mock_profiler)
       expect(mock_profiler).to receive(:prof)
       expect(mock_profiler).to receive(:print_results_flat).with('populate_endpoint')
-
-      subject
+      described_class.populate_endpoint_profiled('fixture_sr1')
     end
   end
 end
