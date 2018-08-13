1ec92054c01ad2d2f4ac66427b7c0c95ecef186f
Johnathan Martin <john.martin@stanford.edu>
2018-08-13 11:44:58 -0700

    make PreservationCatalog::S3::Audit more OO, pass params less

--- app/lib/preservation_catalog/s3/audit.rb
+++ app/lib/preservation_catalog/s3/audit.rb
@@ -4,21 +4,44 @@ module PreservationCatalog
     # are available in the environment.  At the time of this comment, only running queue workers will have proper creds
     # loaded.
     class Audit
-      class << self
-        delegate :bucket, :bucket_name, to: ::PreservationCatalog::S3
+      delegate :bucket, :bucket_name, to: ::PreservationCatalog::S3
 
-        private
+      attr_reader :zmv, :results
 
-        def add_part_not_found_result(results, part)
-          results.add_result(
-            AuditResults::ZIP_PART_NOT_FOUND,
-            endpoint_name: part.zipped_moab_version.zip_endpoint.endpoint_name,
-            s3_key: part.s3_key,
-            bucket_name: bucket_name
-          )
+      # @param [ZippedMoabVersion] the ZippedMoabVersion to check
+      # @param [AuditResults] the AuditResults instance used to track findings for this audit run
+      def initialize(zmv, results)
+        @zmv = zmv
+        @results = results
+      end
+
+      # convenience method for instantiating the audit class and running the check in one call
+      def self.check_aws_replicated_zipped_moab_version(zmv, results)
+        new(zmv, results).check_aws_replicated_zipped_moab_version
+      end
+
+      def check_aws_replicated_zipped_moab_version
+        zmv.zip_parts.where.not(status: :unreplicated).each do |part|
+          aws_s3_object = bucket.object(part.s3_key)
+          next unless check_existence(aws_s3_object, part)
+          next unless compare_checksum_metadata(aws_s3_object, part)
+          part.ok!
         end
+      end
 
-        def add_checksum_mismatch_result(results, part, replicated_checksum)
+      private
+
+      def compare_checksum_metadata(aws_s3_object, part)
+        # NOTE: no checksum computation is happening here (neither on our side, nor on AWS's).  we're just comparing
+        # the checksum we have stored with the checksum we asked AWS to store.  we really don't expect any drift, but
+        # we're here, and it's a cheap check to do, and it'd be weird if they differed, so why not?
+        # TODO: in a later work cycle, we'd like to spot check some cloud archives: that is, pull the zip down,
+        # re-compute the checksum for the retrieved zip, and make sure it matches what we stored.
+        replicated_checksum = aws_s3_object.metadata["checksum_md5"]
+        if part.md5 == replicated_checksum
+          part.update(last_checksum_validation: Time.zone.now)
+          true
+        else
           results.add_result(
             AuditResults::ZIP_PART_CHECKSUM_MISMATCH,
             endpoint_name: part.zipped_moab_version.zip_endpoint.endpoint_name,
@@ -27,57 +50,25 @@ module PreservationCatalog
             replicated_checksum: replicated_checksum,
             bucket_name: bucket_name
           )
-        end
-
-        def check_existence(results, aws_s3_object, part)
-          if aws_s3_object.exists?
-            part.update(last_existence_check: Time.zone.now)
-            true
-          else
-            # TODO: honeybadger alert for this, but keep going
-            add_part_not_found_result(results, part)
-            part.update(status: 'not_found', last_existence_check: Time.zone.now)
-            false
-          end
-        end
-
-        def compare_checksum_metadata(results, aws_s3_object, part)
-          # NOTE: no checksum computation is happening here (neither on our side, nor on AWS's).  we're just comparing
-          # the checksum we have stored with the checksum we asked AWS to store.  we really don't expect any drift, but
-          # we're here, and it's a cheap check to do, and it'd be weird if they differed, so why not?
-          # TODO: in a later work cycle, we'd like to spot check some cloud archives: that is, pull the zip down,
-          # re-compute the checksum for the retrieved zip, and make sure it matches what we stored.
-          replicated_checksum = replicated_checksum(aws_s3_object)
-          if part.md5 == replicated_checksum
-            part.update(last_checksum_validation: Time.zone.now)
-            true
-          else
-            # TODO: honeybadger alert for this, but keep going
-            add_checksum_mismatch_result(results, part, replicated_checksum)
-            part.update(status: 'replicated_checksum_mismatch', last_checksum_validation: Time.zone.now)
-            false
-          end
+          part.update(status: 'replicated_checksum_mismatch', last_checksum_validation: Time.zone.now)
+          false
         end
       end
 
-      # TODO: make this more object oriented, init w/ results, make everything instance
-      # methods, just init and call check_aws_replicated_zipped_moab_version
-      #
-      # @param [ZippedMoabVersion] zmv
-      # @param [AuditResults] results
-      def self.check_aws_replicated_zipped_moab_version(zmv, results)
-        zmv.zip_parts.where.not(status: :unreplicated).each do |part|
-          aws_s3_object = bucket.object(part.s3_key)
-          next unless check_existence(results, aws_s3_object, part)
-          next unless compare_checksum_metadata(results, aws_s3_object, part)
-          part.ok!
+      def check_existence(aws_s3_object, part)
+        if aws_s3_object.exists?
+          part.update(last_existence_check: Time.zone.now)
+          true
+        else
+          results.add_result(
+            AuditResults::ZIP_PART_NOT_FOUND,
+            endpoint_name: part.zipped_moab_version.zip_endpoint.endpoint_name,
+            s3_key: part.s3_key,
+            bucket_name: bucket_name
+          )
+          part.update(status: 'not_found', last_existence_check: Time.zone.now)
+          false
         end
-      end
-
-      # @param [Aws::S3::Object]
-      # @return [String] MD5
-      def self.replicated_checksum(aws_s3_object)
-        aws_s3_object.metadata["checksum_md5"]
       end
     end
   end
