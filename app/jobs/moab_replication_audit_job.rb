a9c69ae85462b57ac3edd46adbb7ba253eb5637c
Johnathan Martin <john.martin@stanford.edu>
2018-08-15 14:35:25 -0700

    MoabReplicationAuditJob should check a setting to see whether ZMVs should get backfilled for the CompleteMoab being audited

--- app/jobs/moab_replication_audit_job.rb
+++ app/jobs/moab_replication_audit_job.rb
@@ -12,16 +12,8 @@ class MoabReplicationAuditJob < ApplicationJob
     druid = complete_moab.preserved_object.druid
 
     results = AuditResults.new(druid, nil, complete_moab.moab_storage_root, "MoabReplicationAuditJob")
-    # TODO: will also need to create a CompleteMoab.archive_check_expired scope, use that
-    # to queue jobs for this worker.
 
-    backfilled_zmvs = complete_moab.create_zipped_moab_versions!
-    unless backfilled_zmvs.empty?
-      results.add_result(
-        AuditResults::ZMV_BACKFILL,
-        version_endpoint_pairs: format_backfilled_zmvs(backfilled_zmvs)
-      )
-    end
+    backfill_missing_zmvs(complete_moab, results) if Settings.replication.audit_should_backfill
 
     complete_moab.zipped_moab_versions.each do |zmv|
       next unless check_child_zip_part_attributes(zmv, results)
@@ -34,6 +26,16 @@ class MoabReplicationAuditJob < ApplicationJob
   end
 
   private
+
+  def backfill_missing_zmvs(complete_moab, results)
+    backfilled_zmvs = complete_moab.create_zipped_moab_versions!
+    return if backfilled_zmvs.empty?
+
+    results.add_result(
+      AuditResults::ZMV_BACKFILL,
+      version_endpoint_pairs: format_backfilled_zmvs(backfilled_zmvs)
+    )
+  end
 
   def format_backfilled_zmvs(backfilled_zmvs)
     backfilled_zmvs.map { |bz| "#{bz.version} to #{bz.zip_endpoint.endpoint_name}" }.sort.join("; ")
--- spec/jobs/moab_replication_audit_job_spec.rb
+++ spec/jobs/moab_replication_audit_job_spec.rb
@@ -8,6 +8,7 @@ describe MoabReplicationAuditJob, type: :job do
   before do
     allow(AuditResults).to receive(:new).and_return(results)
     allow(results).to receive(:report_results)
+    allow(Settings.replication).to receive(:audit_should_backfill).and_return(true) # default to enabled for tests
   end
 
   describe '#perform' do
@@ -22,10 +23,21 @@ describe MoabReplicationAuditJob, type: :job do
     context 'there are zipped_moab_versions to backfill for complete_moab' do
       before { cm.zipped_moab_versions.destroy_all } # undo auto-spawned rows from callback
 
-      it 'logs a warning about uncreated ZMVs' do
+      it 'tries to create missing ZMVs and logs a warning about uncreated ZMVs' do
+        expect(cm).to receive(:create_zipped_moab_versions!).and_call_original
         job.perform(cm)
         msg = "backfilled the following ZippedMoabVersions: 1 to mock_archive1; 2 to mock_archive1"
         expect(results.result_array).to include(a_hash_including(AuditResults::ZMV_BACKFILL => msg))
+      end
+
+      context 'automatic backfilling is disabled' do
+        before { allow(Settings.replication).to receive(:audit_should_backfill).and_return(false) }
+
+        it 'does not try to create ZippedMoabVersions for the CompleteMoab' do
+          expect(cm).not_to receive(:create_zipped_moab_versions!)
+          job.perform(cm)
+          expect(results.result_array).not_to include(a_hash_including(AuditResults::ZMV_BACKFILL))
+        end
       end
     end
 
