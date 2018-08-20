193de0a6cf0fb8b97d6f9591f96cdcfe1b9c0f8e
Tony Zanella <tony.zanella@gmail.com>
2018-08-20 10:57:45 -0700

    Remove nonexistent role in stage deploy

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -1,5 +1,5 @@
 server 'preservation-catalog-stage-01.stanford.edu', user: 'pres', roles: %w[app db web]
-server 'preservation-catalog-stage-02.stanford.edu', user: 'pres', roles: %w[worker app resque]
+server 'preservation-catalog-stage-02.stanford.edu', user: 'pres', roles: %w[resque app]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
