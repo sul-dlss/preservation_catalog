b6e86a240989637c1f0c4e9c8b745c90986438dc
Tony Zanella <tony.zanella@gmail.com>
2018-08-26 12:21:55 -0700

    Monkeypatch to silence deprecation warning

--- config/initializers/resque.rb
+++ config/initializers/resque.rb
@@ -7,3 +7,16 @@ Resque.redis = resque_config[Rails.env.to_s]
 
 # configure a separate failure queue per job queue
 Resque::Failure.backend = Resque::Failure::RedisMultiQueue
+
+# see https://github.com/resque/resque/issues/1591#issuecomment-403805957
+# this silences a deprecation warning that was hogging disk space
+# and disrupting replication. Another suggested solution was pinning
+# the resque gem to current master, but that didn't silence the warning
+# compare https://travis-ci.org/sul-dlss/preservation_catalog/builds/420832575
+# with    https://travis-ci.org/sul-dlss/preservation_catalog/builds/420830070
+# the former monkeypatch branch shows no warnings, the latter, pinning, does
+Redis::Namespace.class_eval do
+  def client
+    _client
+  end
+end
