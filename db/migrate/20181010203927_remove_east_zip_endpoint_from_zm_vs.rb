f8cbed69682f4f21e627eb0a13b6f5c63ec9bab6
Sarav Shah <saravsshah@gmail.com>
2018-10-11 09:42:32 -0700

    disable DDL transaction

--- db/migrate/20181010203927_remove_east_zip_endpoint_from_zm_vs.rb
+++ db/migrate/20181010203927_remove_east_zip_endpoint_from_zm_vs.rb
@@ -1,5 +1,6 @@
 class RemoveEastZipEndpointFromZmVs < ActiveRecord::Migration[5.1]
   def change
+    disable_ddl_transaction!
     east_endpoint = ZipEndpoint.find_by(endpoint_name: "aws_s3_east_1")
     east_endpoint.zipped_moab_versions.find_each(&:destroy)
     east_endpoint.destroy
