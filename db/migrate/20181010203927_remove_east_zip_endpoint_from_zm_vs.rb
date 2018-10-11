6bf7fe485c9bc9f1c5d91d079546d96210c83df3
Sarav Shah <saravsshah@gmail.com>
2018-10-11 11:33:02 -0700

    Runnin the deletion through the console

--- db/migrate/20181010203927_remove_east_zip_endpoint_from_zm_vs.rb
+++ db/migrate/20181010203927_remove_east_zip_endpoint_from_zm_vs.rb
@@ -1,7 +1,8 @@
-class RemoveEastZipEndpointFromZmVs < ActiveRecord::Migration[5.1]
-  def change
-    east_endpoint = ZipEndpoint.find_by(endpoint_name: "aws_s3_east_1")
-    east_endpoint.zipped_moab_versions.find_each(&:destroy)
-    east_endpoint.destroy
-  end
-end
+# Commenting out so this stays in the history, turns out we need to run these commands in the console.
+# class RemoveEastZipEndpointFromZmVs < ActiveRecord::Migration[5.1]
+#   def change
+#     east_endpoint = ZipEndpoint.find_by(endpoint_name: "aws_s3_east_1")
+#     east_endpoint.zipped_moab_versions.find_each(&:destroy)
+#     east_endpoint.destroy
+#   end
+# end
