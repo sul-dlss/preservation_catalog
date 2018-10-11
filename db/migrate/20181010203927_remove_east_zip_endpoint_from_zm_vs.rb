db67c60367957528f0caadc838bd0412eb532305
Johnathan Martin <john.martin@stanford.edu>
2018-10-11 11:42:18 -0700

    uncomment class definition so that migration runs, but leave contents commented out

--- db/migrate/20181010203927_remove_east_zip_endpoint_from_zm_vs.rb
+++ db/migrate/20181010203927_remove_east_zip_endpoint_from_zm_vs.rb
@@ -1,8 +1,8 @@
 # Commenting out so this stays in the history, turns out we need to run these commands in the console.
-# class RemoveEastZipEndpointFromZmVs < ActiveRecord::Migration[5.1]
+class RemoveEastZipEndpointFromZmVs < ActiveRecord::Migration[5.1]
 #   def change
 #     east_endpoint = ZipEndpoint.find_by(endpoint_name: "aws_s3_east_1")
 #     east_endpoint.zipped_moab_versions.find_each(&:destroy)
 #     east_endpoint.destroy
 #   end
-# end
+end
