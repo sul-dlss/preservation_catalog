f44af0eebbd0c3c787fb746729a4282ff4e86229
Joe Atzberger <ohiocore@gmail.com>
2018-10-10 22:46:18 -0700

    Convert MoabReplicationAuditJob to use PartReplicationAuditJob
    No longer uses `AuditResults` to accumulate "results", since it only has a single log message
    possible to send in one place.  We know the severity, we know we *don't*
    need to alert honeybadger, and we know the message.  We don't need
    another class involved.
    
    `AuditResults` still used at the part level check.

--- app/jobs/moab_replication_audit_job.rb
+++ app/jobs/moab_replication_audit_job.rb
@@ -1,43 +1,34 @@
-# Confirms that a CompleteMoab is fully/properly replicated to all target zip endpoints.
-# Usage info:
-# MoabReplicationAuditJob.perform_later(cm)
+# Confirms a CompleteMoab has all versions/parts replicated for each of its target endpoints.
+# @note Enqueues a check per endpoint
+# @example usage
+#   MoabReplicationAuditJob.perform_later(cm)
 class MoabReplicationAuditJob < ApplicationJob
   queue_as :moab_replication_audit
-  delegate :check_child_zip_part_attributes, to: Audit::CatalogToArchive
-  delegate :check_aws_replicated_zipped_moab_version, to: PreservationCatalog::S3::Audit
-  delegate :logger, to: Audit::CatalogToArchive
 
   # @param [CompleteMoab] verify that the zip exists on the endpoint
   def perform(complete_moab)
-    druid = complete_moab.preserved_object.druid
-
-    results = AuditResults.new(druid, nil, complete_moab.moab_storage_root, "MoabReplicationAuditJob")
-
-    backfill_missing_zmvs(complete_moab, results) if Settings.replication.audit_should_backfill
-
-    complete_moab.zipped_moab_versions.each do |zmv|
-      next unless check_child_zip_part_attributes(zmv, results)
-      check_aws_replicated_zipped_moab_version(zmv, results)
-    end
-
+    backfill_missing_zmvs(complete_moab)
+    ZipEndpoint
+      .includes(:zipped_moab_versions)
+      .where(zipped_moab_versions: { complete_moab: complete_moab }).each do |endpoint|
+        PartReplicationAuditJob.perform_later(complete_moab, endpoint)
+      end
     complete_moab.update(last_archive_audit: Time.current)
-
-    results.report_results(logger)
   end
 
   private
 
-  def backfill_missing_zmvs(complete_moab, results)
-    backfilled_zmvs = complete_moab.create_zipped_moab_versions!
-    return if backfilled_zmvs.empty?
-
-    results.add_result(
-      AuditResults::ZMV_BACKFILL,
-      version_endpoint_pairs: format_backfilled_zmvs(backfilled_zmvs)
+  def backfill_missing_zmvs(complete_moab)
+    return unless Settings.replication.audit_should_backfill
+    zmvs = complete_moab.create_zipped_moab_versions!
+    return if zmvs.empty?
+    Audit::CatalogToArchive.logger.warn(
+      "#{self.class}: #{complete_moab.preserved_object.druid} backfilled #{zmvs.count} ZippedMoabVersions: #{format_zmvs(zmvs)}"
     )
   end
 
-  def format_backfilled_zmvs(backfilled_zmvs)
-    backfilled_zmvs.map { |bz| "#{bz.version} to #{bz.zip_endpoint.endpoint_name}" }.sort.join("; ")
+  # @return [String] a potentially large message
+  def format_zmvs(zmvs)
+    zmvs.map { |bz| "#{bz.version} to #{bz.zip_endpoint.endpoint_name}" }.sort.join("; ")
   end
 end
