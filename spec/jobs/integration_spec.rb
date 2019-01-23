6720c1e6d25896d03bdc76df41e653feb9642e24
Tony Zanella <tony.zanella@gmail.com>
2019-01-22 17:45:13 -0800

    Fixed results_recorder_job

--- spec/jobs/integration_spec.rb
+++ spec/jobs/integration_spec.rb
@@ -7,8 +7,9 @@ describe 'the whole replication pipeline', type: :job do # rubocop:disable RSpec
   let(:druid) { zmv.preserved_object.druid }
   let(:version) { zmv.version }
   let(:deliverer) { zmv.zip_endpoint.delivery_class.to_s }
+  let(:ibm_deliverer) { 'IbmSouthDeliveryJob' }
   let(:hash) do
-    { druid: druid, version: version, zip_endpoints: [zmv.zip_endpoint.endpoint_name] }
+    { druid: druid, version: version, zip_endpoints: ['ibm_us_south', zmv.zip_endpoint.endpoint_name] }
   end
   let(:s3_key) { 'bj/102/hs/9687/bj102hs9687.v0001.zip' }
 
@@ -23,13 +24,16 @@ describe 'the whole replication pipeline', type: :job do # rubocop:disable RSpec
     FactoryBot.reload # we need the "first" PO, bj102hs9687, for PC to line up w/ fixture
     allow(Settings).to receive(:zip_storage).and_return(Rails.root.join('spec', 'fixtures', 'zip_storage'))
     allow(PreservationCatalog::S3).to receive(:bucket).and_return(bucket)
+    allow(PreservationCatalog::Ibm).to receive(:bucket).and_return(bucket)
   end
 
   it 'gets from zipmaker queue to replication result message' do
     expect(PlexerJob).to receive(:perform_later).with(druid, version, s3_key, Hash).and_call_original
     expect(S3WestDeliveryJob).to receive(:perform_later).with(druid, version, s3_key, Hash).and_call_original
+    expect(IbmSouthDeliveryJob).to receive(:perform_later).with(druid, version, s3_key, Hash).and_call_original
     # other endpoints as added...
     expect(ResultsRecorderJob).to receive(:perform_later).with(druid, version, s3_key, deliverer).and_call_original
+    expect(ResultsRecorderJob).to receive(:perform_later).with(druid, version, s3_key, ibm_deliverer).and_call_original
     expect(Resque.redis.redis).to receive(:lpush).with('replication.results', hash.to_json)
     ZipmakerJob.perform_now(druid, version)
   end
