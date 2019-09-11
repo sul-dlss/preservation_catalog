0700d1efbfbf00418d941132210fbc875b5f58cf
Peter Mangiafico <peter@mangiafico.org>
2019-09-11 10:00:55 -0700

    add csv format for multi-druid endpoint

--- app/controllers/objects_controller.rb
+++ app/controllers/objects_controller.rb
@@ -1,5 +1,7 @@
 # frozen_string_literal: true
 
+require 'csv'
+
 ##
 # ObjectsController allows consumers to interact with preserved objects
 #  (Note: methods will eventually be ported from sdr-services-app)
@@ -9,7 +11,7 @@ class ObjectsController < ApplicationController
   def show
     object = PreservedObject.find_by(druid: strip_druid(params[:id]))
     if object
-      render status: 200, json: object.to_json
+      render json: object.to_json
     else
       render status: 404, json: 'Object not found'
     end
@@ -18,7 +20,7 @@ class ObjectsController < ApplicationController
   # return the checksums and filesize for a single druid (supplied with druid: prefix)
   # GET /objects/:druid/checksum
   def checksum
-    render status: 200, json: checksum_for_object(params[:id]).to_json
+    render json: checksum_for_object(params[:id]).to_json
   rescue Moab::ObjectNotFoundException => e
     render status: 404, json: e.message
   rescue StandardError => e
@@ -28,8 +30,24 @@ class ObjectsController < ApplicationController
   # return the checksums and filesize for a list of druid (supplied with druid: prefix)
   # GET /objects/checksums?druids=druida,druidb,druidc
   def checksums
-    results = params[:druids].map { |druid| checksum_for_object(druid) }
-    render status: 200, json: results.to_json
+    druids = params[:druids]
+    respond_to do |format|
+      format.json do
+        results = druids.map { |druid| { "#{druid}": checksum_for_object(druid) } }
+        render json: results.to_json
+      end
+      format.csv do
+        results = CSV.generate do |csv|
+          druids.each do |druid|
+            checksum_for_object(druid).each do |checksum|
+              csv << [druid, checksum[:filename], checksum[:md5], checksum[:sha1], checksum[:sha256], checksum[:filesize]]
+            end
+          end
+        end
+        render plain: results
+      end
+      format.any  { render status: 406, plain: 'Format not acceptable' }
+    end
   rescue Moab::ObjectNotFoundException => e
     render status: 404, json: e.message
   rescue StandardError => e
