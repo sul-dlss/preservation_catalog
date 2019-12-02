40291c0e091936c6121a20827fc635579ec70c0d
Naomi Dushay <ndushay@stanford.edu>
2019-12-02 15:41:43 -0800

    MoabStorageService.filename perfected (ha)

--- app/services/moab_storage_service.rb
+++ app/services/moab_storage_service.rb
@@ -2,18 +2,17 @@
 
 # Responsibility: interactions with Moab storage to support read-only access for ReST API calls
 class MoabStorageService
+  # @return [Pathname] Pathname object containing the full path for the specified file
+  #  raises ArgumentError or Moab::MoabRuntimeError as appropriate
   # @param [String] druid
   # @param [String] category category of desired file ('content', 'metadata', or 'manifest')
   # @param [String] filename name of the file (path relative to base directory)
-  # @param [String] version of the file (nil = latest)
-  # @return [Pathname] Pathname object containing the full path for the specified file
-  # @raise [Moab::ObjectNotFoundException] if file not found
-  def self.retrieve_file(druid, category, filename, version=nil)
-    raise(ArgumentError, "No filename provided to MoabStorageService.retrieve_file for druid #{druid}") if filename.blank?
-    err_msg = "category arg must be 'content', 'metadata', or 'manifest' (MoabStorageService.retrieve_file for druid #{druid})"
+  # @param [Integer, nil] version of the file (nil = latest)
+  def self.filepath(druid, category, filename, version=nil)
+    raise(ArgumentError, "No filename provided to MoabStorageService.filepath for druid #{druid}") if filename.blank?
+    err_msg = "category arg must be 'content', 'metadata', or 'manifest' (MoabStorageService.filepath for druid #{druid})"
     raise(ArgumentError, err_msg) unless ['content', 'metadata', 'manifest'].include?(category)
-    file_pathname = Stanford::StorageServices.retrieve_file(category, filename, druid, version)
-    File.open(file_pathname).read
+    Stanford::StorageServices.retrieve_file(category, filename, druid, version)
   end
 
   def self.retrieve_content_file_group(druid)
