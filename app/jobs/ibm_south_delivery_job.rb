5067d1e1d77a3d3ba0c1632c344c9d83421a25d6
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-01-07 15:26:17 -0800

    Use IBM buckets in IBM job

--- app/jobs/ibm_south_delivery_job.rb
+++ app/jobs/ibm_south_delivery_job.rb
@@ -10,6 +10,6 @@ class IbmSouthDeliveryJob < S3WestDeliveryJob
       access_key_id: Settings.zip_endpoints.ibm_us_south.access_key_id,
       secret_access_key: Settings.zip_endpoints.ibm_us_south.secret_access_key
     )
-    PreservationCatalog::S3.bucket
+    PreservationCatalog::Ibm.bucket
   end
 end
