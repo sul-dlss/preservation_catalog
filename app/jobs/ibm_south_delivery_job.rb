ed5c825920613948760cb0cff31c6599d10e097e
Johnathan Martin <john.martin@stanford.edu>
2020-01-17 15:30:23 -0800

    fix comments re: S3 creds, useful example query for db readme
    * former in light of switch from provisioning creds via environment variables
    * latter because it was useful when i was looking into a replication failure, and is also a nice example of a multi-table join in ActiveRecord

--- app/jobs/ibm_south_delivery_job.rb
+++ app/jobs/ibm_south_delivery_job.rb
@@ -2,7 +2,7 @@
 
 # Different provider than parent class
 class IbmSouthDeliveryJob < S3WestDeliveryJob
-  queue_as :ibm_us_south_delivery # note: as with Amazon, still needs proper ENVs for AWS_REGION, etc.
+  queue_as :ibm_us_south_delivery
 
   def bucket
     PreservationCatalog::Ibm.configure(
