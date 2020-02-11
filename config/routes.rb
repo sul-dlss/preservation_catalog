8355a968ada41c654cfdb5c7677f4cf7d579a39e
Naomi Dushay <ndushay@stanford.edu>
2020-02-11 13:29:31 -0800

    remove unversioned routes

--- config/routes.rb
+++ config/routes.rb
@@ -21,13 +21,4 @@ Rails.application.routes.draw do
       end
     end
   end
-
-  # TODO: Remove these when all clients are using the version-scoped routes above
-  post '/catalog', to: redirect('/v1/catalog')
-  match '/catalog/:druid', to: redirect('/v1/catalog/%{druid}'), via: %i[patch put]
-  get '/objects/:id/checksum', to: redirect('/v1/objects/%{id}/checksum')
-  get '/objects/:id/file', to: redirect('/v1/objects/%{id}/file')
-  post '/objects/:id/content_diff', to: redirect('/v1/objects/%{id}/content_diff')
-  match '/objects/checksums', to: redirect('/v1/objects/checksums'), via: %i[get post]
-  get '/objects/:id', to: redirect('/v1/objects/%{id}')
 end
