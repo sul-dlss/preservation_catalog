d0920fb009e3bb381d3690055925a414b1b73615
Naomi Dushay <ndushay@stanford.edu>
2020-02-18 13:16:30 -0800

    comment out weekend cron jobs

--- config/schedule.rb
+++ config/schedule.rb
@@ -12,14 +12,14 @@ every :wednesday, roles: [:queue_populator] do
   set :output, standard: nil, error: 'log/c2a-err.log'
   runner 'CompleteMoab.archive_check_expired.find_each(&:audit_moab_version_replication!)'
 end
-every :friday, roles: [:queue_populator] do
-  set :output, standard: nil, error: 'log/c2m-err.log'
-  runner 'MoabStorageRoot.find_each(&:c2m_check!)'
-end
-every :sunday, at: '1am', roles: [:queue_populator] do
-  set :output, standard: nil, error: 'log/cv-err.log'
-  runner 'MoabStorageRoot.find_each(&:validate_expired_checksums!)'
-end
+# every :friday, roles: [:queue_populator] do
+#   set :output, standard: nil, error: 'log/c2m-err.log'
+#   runner 'MoabStorageRoot.find_each(&:c2m_check!)'
+# end
+# every :sunday, at: '1am', roles: [:queue_populator] do
+#   set :output, standard: nil, error: 'log/cv-err.log'
+#   runner 'MoabStorageRoot.find_each(&:validate_expired_checksums!)'
+# end
 
 every :hour, roles: [:cache_cleaner] do
   set :output, standard: '/var/log/preservation_catalog/zip_cache_cleanup.log'
