216b2520b3e5fe502baa1673bf22814c47e0095a
Jeremy Nelson <jermnelson@gmail.com>
2020-02-19 14:51:56 -0700

    Move OKComputerController logger constant to Rails.configuration.to_prepare

--- config/initializers/okcomputer.rb
+++ config/initializers/okcomputer.rb
@@ -5,7 +5,9 @@ require Rails.root.join('config', 'initializers', 'resque.rb').to_s
 
 OkComputer.mount_at = 'status' # use /status or /status/all or /status/<name-of-check>
 OkComputer.check_in_parallel = true
-OkComputer::OkComputerController.logger = ActiveSupport::Logger.new(File.join('log', 'ok_computer_controller.log'))
+Rails.configuration.to_prepare do
+  OkComputer::OkComputerController.logger = ActiveSupport::Logger.new(File.join('log', 'ok_computer_controller.log'))
+end
 
 # check models to see if at least they have some data
 class TablesHaveDataCheck < OkComputer::Check
