b9ba11daab3c8251c64d4ec1db776eac18e54b18
Johnathan Martin <john.martin@stanford.edu>
2020-03-23 18:59:46 -0700

    re-enable read-only routes
    per https://github.com/sul-dlss/preservation_catalog/issues/1439#issuecomment-600759173 we won't shut down accessioning robots for migration, which means that we'll need to enable the routes that allow accessioning robots to talk to pres cat for their read-only requests.
    
    we'll leave disabled the create and update endpoints, but only the preservation robots should use those (and the preservation robots will still be disabled).

--- config/routes.rb
+++ config/routes.rb
@@ -7,18 +7,18 @@ Rails.application.routes.draw do
         at: '/resque',
         constraints: ->(req) { Settings.resque_dashboard_hostnames.include?(req.host) }
 
-  # scope 'v1' do
-  #   resources :catalog, param: :druid, only: %i[create update]
-  #
-  #   resources :objects, only: %i[show] do
-  #     member do
-  #       get 'checksum'
-  #       get 'file', format: false # no need to add extension to url
-  #       post 'content_diff'
-  #     end
-  #     collection do
-  #       match 'checksums', via: %i[get post]
-  #     end
-  #   end
-  # end
+  scope 'v1' do
+    # resources :catalog, param: :druid, only: %i[create update]
+
+    resources :objects, only: %i[show] do
+      member do
+        get 'checksum'
+        get 'file', format: false # no need to add extension to url
+        post 'content_diff'
+      end
+      collection do
+        match 'checksums', via: %i[get post]
+      end
+    end
+  end
 end
