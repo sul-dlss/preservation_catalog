e1f3654699e3b7a88bcb11750eb361bee568f13c
Johnathan Martin <john.martin@stanford.edu>
2020-06-18 23:26:34 -0700

    fix replication integration spec for zipped_moab_versions data model change, plus touchups
    also:
    * simplify let var setup to make it easier to set test expectations without referencing the (yet-to-be-created) CompleteMoab
    * make test expectations a bit more exact and a bit more explicit
    * trigger integration pipeline a bit more realistically

--- spec/jobs/integration_spec.rb
+++ spec/jobs/integration_spec.rb
@@ -7,26 +7,21 @@ describe 'the whole replication pipeline', type: :job do
   let(:ibm_s3_object) { instance_double(::Aws::S3::Object, exists?: false, upload_file: true) }
   let(:aws_bucket) { instance_double(::Aws::S3::Bucket, object: aws_s3_object) }
   let(:ibm_bucket) { instance_double(::Aws::S3::Bucket, object: ibm_s3_object) }
-  let(:cm) { create(:complete_moab) }
-  let(:zmv) { cm.zipped_moab_versions.first! }
-  let(:zmv2) { cm.zipped_moab_versions.second! }
-  let(:druid) { zmv.preserved_object.druid }
-  let(:version) { zmv.version }
-  let(:deliverer) { zmv.zip_endpoint.delivery_class.to_s }
-  let(:deliverer2) { zmv2.zip_endpoint.delivery_class.to_s }
+  let(:preserved_object) { create(:preserved_object) }
+  let(:druid) { preserved_object.druid }
+  let(:version) { preserved_object.current_version }
+  let(:zip_endpoints) { preserved_object.preservation_policy.zip_endpoints }
   let(:hash) do
     {
       druid: druid,
       version: version,
-      zip_endpoints: [
-        zmv.zip_endpoint.endpoint_name,
-        zmv2.zip_endpoint.endpoint_name
-      ].sort
+      zip_endpoints: zip_endpoints.map(&:endpoint_name).sort
     }
   end
   let(:s3_key) { 'bj/102/hs/9687/bj102hs9687.v0001.zip' }
   let(:aws_provider) { instance_double(PreservationCatalog::AwsProvider, bucket: aws_bucket) }
   let(:ibm_provider) { instance_double(PreservationCatalog::IbmProvider, bucket: ibm_bucket) }
+  let(:moab_storage_root) { create(:moab_storage_root) }
 
   around do |example|
     old_adapter = ApplicationJob.queue_adapter
@@ -43,13 +38,16 @@ describe 'the whole replication pipeline', type: :job do
   end
 
   it 'gets from zipmaker queue to replication result message' do
+    expect(ZipmakerJob).to receive(:perform_later).with(druid, version, moab_storage_root.storage_location).and_call_original
     expect(PlexerJob).to receive(:perform_later).with(druid, version, s3_key, Hash).and_call_original
     expect(S3WestDeliveryJob).to receive(:perform_later).with(druid, version, s3_key, Hash).and_call_original
     expect(IbmSouthDeliveryJob).to receive(:perform_later).with(druid, version, s3_key, Hash).and_call_original
     # other endpoints as added...
-    expect(ResultsRecorderJob).to receive(:perform_later).with(druid, version, s3_key, deliverer).and_call_original
-    expect(ResultsRecorderJob).to receive(:perform_later).with(druid, version, s3_key, deliverer2).and_call_original
+    expect(ResultsRecorderJob).to receive(:perform_later).with(druid, version, s3_key, 'S3WestDeliveryJob').and_call_original
+    expect(ResultsRecorderJob).to receive(:perform_later).with(druid, version, s3_key, 'IbmSouthDeliveryJob').and_call_original
     expect(Resque.redis.redis).to receive(:lpush).with('replication.results', hash.to_json)
-    ZipmakerJob.perform_now(druid, version)
+
+    # creating or updating a CompleteMoab should trigger its parent PreservedObject to replicate any missing versions to any target endpoints
+    create(:complete_moab, preserved_object: preserved_object, version: preserved_object.current_version, moab_storage_root: moab_storage_root)
   end
 end
