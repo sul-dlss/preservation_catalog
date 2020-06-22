c9bb5517e809e54d0a3135b6bfca62c9c36e2438
Johnathan Martin <john.martin@stanford.edu>
2020-06-22 11:01:52 -0700

    move DruidVersionJobBase code into consuming classes to allow modification according to diverging needs

--- app/jobs/zipmaker_job.rb
+++ app/jobs/zipmaker_job.rb
@@ -4,9 +4,20 @@
 # If needed, zip files to zip storage and calculate checksum(s).
 # Otherwise, touch the existing main ".zip" file to freshen it in cache.
 # Invoke PlexerJob for each zip part.
-class ZipmakerJob < DruidVersionJobBase
+class ZipmakerJob < ApplicationJob
   queue_as :zipmaker
   delegate :create_zip!, :file_path, :part_keys, to: :zip
+
+  attr_accessor :zip
+
+  before_perform do |job|
+    job.zip = DruidVersionZip.new(job.arguments.first, job.arguments.second)
+  end
+
+  # Does queue locking on ONLY druid and version (as first and second parameters)
+  def self.lock(*args)
+    "lock:#{name}-#{args.slice(0..1)}"
+  end
 
   # @param [String] druid
   # @param [Integer] version
