c400edb3e160343aa7dd6145dd6b62298211a519
Michael J. Giarlo <mjgiarlo@stanford.edu>
2020-08-10 16:01:05 -0700

    Add QA deployment target

---
+++ config/deploy/qa.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+server 'preservation-catalog-qa-01.stanford.edu', user: 'pres', roles: %w[app db web]
+server 'preservation-catalog-qa-02.stanford.edu', user: 'pres', roles: %w[resque app]
+
+Capistrano::OneTimeKey.generate_one_time_key!
+set :rails_env, 'production'
+set :bundle_without, 'deploy test'
+set :deploy_to, '/opt/app/pres/preservation_catalog'
+append :linked_files, 'config/newrelic.yml', 'config/resque.yml'
