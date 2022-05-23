18a8f2bffbfb8bac86a10012ae1ddf385f8a02c3
Justin Coyne <jcoyne@justincoyne.com>
2022-05-23 11:51:06 -0500

    Remove app role from preservation-catalog-stage-02
    app is for rails application servers.

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,7 +2,7 @@
 
 server 'preservation-catalog-web-stage-01.stanford.edu', user: 'pres', roles: %w[app web]
 server 'preservation-catalog-web-stage-02.stanford.edu', user: 'pres', roles: %w[app web]
-server 'preservation-catalog-stage-02.stanford.edu', user: 'pres', roles: %w[app db resque queue_populator cache_cleaner]
+server 'preservation-catalog-stage-02.stanford.edu', user: 'pres', roles: %w[db resque queue_populator cache_cleaner]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
