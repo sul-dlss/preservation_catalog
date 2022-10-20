8b7a3822d31a74aab8126d51191084ea02465ab5
Michael J. Giarlo <mjgiarlo@stanford.edu>
2022-10-20 12:12:49 -0700

    Refactor zip delivery
    Fixes #1922
    
    This commit refactors zip delivery in the following ways:
    
    * Move zip delivery into its own small service class
    * Invoke zip delivery service from a new abstract delivery job
    * Base all three delivery jobs on the abstract delivery job

---
+++ spec/jobs/abstract_delivery_job_spec.rb
@@ -0,0 +1,52 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe AbstractDeliveryJob, type: :job do
+  # NOTE: Test a concrete impl of the abstract job so the call to `#bucket` does not raise
+  subject(:job_implementation) do
+    Class.new(described_class) do
+      def bucket
+        Struct.new(:unused) do
+          def object(arg); end
+        end.new
+      end
+    end
+  end
+
+  let(:druid) { 'bj102hs9687' }
+  let(:version) { 1 }
+  let(:dvz) { DruidVersionZip.new(druid, version) }
+  let(:dvz_part) { DruidVersionZipPart.new(dvz, part_s3_key) }
+  let(:metadata) { dvz_part.metadata.merge(zip_version: 'Zip 3.0 (July 5th 2008)') }
+  let(:part_s3_key) { dvz.s3_key('.zip') }
+  let(:delivery_result) { true }
+
+  before do
+    allow(Settings).to receive(:zip_storage).and_return(Rails.root.join('spec', 'fixtures', 'zip_storage'))
+    allow(ResultsRecorderJob).to receive(:perform_later)
+    allow(ZipDeliveryService).to receive(:deliver).and_return(delivery_result)
+
+    job_implementation.perform_now(druid, version, part_s3_key, metadata)
+  end
+
+  context 'when zip delivery succeeds' do
+    it 'invokes ResultsRecorderJob' do
+      expect(ResultsRecorderJob).to have_received(:perform_later).with(druid, version, part_s3_key, job_implementation.to_s)
+    end
+  end
+
+  context 'when zip delivery fails' do
+    let(:delivery_result) { nil }
+
+    it 'does nothing' do
+      expect(ResultsRecorderJob).not_to have_received(:perform_later)
+    end
+  end
+
+  describe '#bucket' do
+    it 'raises NotImplementedError' do
+      expect { described_class.new.bucket }.to raise_error(NotImplementedError)
+    end
+  end
+end
---
+++ spec/services/zip_delivery_service_spec.rb
@@ -0,0 +1,63 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe ZipDeliveryService do
+  let(:instance) { described_class.new(s3_part: s3_part, dvz_part: dvz_part, metadata: metadata) }
+  let(:druid) { 'bj102hs9687' }
+  let(:version) { 1 }
+  let(:dvz) { DruidVersionZip.new(druid, version) }
+  let(:dvz_part) { DruidVersionZipPart.new(dvz, part_s3_key) }
+  let(:s3_part) { instance_double(::Aws::S3::Object, exists?: part_exists, upload_file: true, key: part_s3_key) }
+  let(:part_exists) { false }
+  let(:md5) { '4f98f59e877ecb84ff75ef0fab45bac5' }
+  let(:base64) { dvz.hex_to_base64(md5) }
+  let(:metadata) { dvz_part.metadata.merge(zip_version: 'Zip 3.0 (July 5th 2008)') }
+  let(:part_s3_key) { dvz.s3_key('.zip') }
+
+  before do
+    allow(Settings).to receive(:zip_storage).and_return(Rails.root.join('spec', 'fixtures', 'zip_storage'))
+    allow(IO).to receive(:read).with(dvz_part.md5_path).and_return(md5)
+  end
+
+  describe '.deliver' do
+    before do
+      allow(described_class).to receive(:new).and_return(instance)
+      allow(instance).to receive(:deliver)
+    end
+
+    it 'invokes #deliver on a new instance' do
+      described_class.deliver(s3_part: s3_part, dvz_part: dvz_part, metadata: metadata)
+      expect(instance).to have_received(:deliver).once
+    end
+  end
+
+  describe '#deliver' do
+    context 'when s3 part exists' do
+      let(:part_exists) { true }
+
+      it 'returns nil and does not upload the file' do
+        expect(instance.deliver).to be_nil
+        expect(s3_part).not_to have_received(:upload_file)
+      end
+    end
+
+    context 'when s3 part does not exist' do
+      context 'when checksums do not match' do
+        let(:md5) { nil }
+
+        it 'raises a RuntimeError' do
+          expect { instance.deliver }.to raise_error(/bj102hs9687.v0001.zip MD5 mismatch/)
+          expect(s3_part).not_to have_received(:upload_file)
+        end
+      end
+
+      it 'delivers the zip' do
+        instance.deliver
+        expect(s3_part).to have_received(:upload_file).with(
+          dvz_part.file_path, metadata: a_hash_including(checksum_md5: md5)
+        )
+      end
+    end
+  end
+end
