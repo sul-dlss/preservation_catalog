3209ea380dc3f84b97413887b1b259136a3914f5
Johnathan Martin <jmartin-sul@users.noreply.github.com>
2022-11-18 10:06:56 -0800

    QA: update cap deploy config to use load balanced web hosts

--- config/deploy/qa.rb
+++ config/deploy/qa.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
 
-server 'preservation-catalog-qa-01.stanford.edu', user: 'pres', roles: %w[app db web]
-server 'preservation-catalog-qa-02.stanford.edu', user: 'pres', roles: %w[app resque queue_populator cache_cleaner]
+server 'preservation-catalog-web-qa-01.stanford.edu', user: 'pres', roles: %w[app web]
+server 'preservation-catalog-web-qa-02.stanford.edu', user: 'pres', roles: %w[app web]
+server 'preservation-catalog-qa-02.stanford.edu', user: 'pres', roles: %w[app db resque queue_populator cache_cleaner]
 
 Capistrano::OneTimeKey.generate_one_time_key!
 set :rails_env, 'production'
