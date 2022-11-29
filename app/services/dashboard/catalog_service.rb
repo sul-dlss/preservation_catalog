9dfd327313256dd85938045981b39689143d280b
Naomi Dushay <ndushay@stanford.edu>
2022-11-29 11:14:20 -0800

    catalog_service: add comment indicating it's about on prem storage

--- app/services/dashboard/catalog_service.rb
+++ app/services/dashboard/catalog_service.rb
@@ -4,7 +4,7 @@ require 'action_view' # for number_to_human_size
 
 # services for dashboard
 module Dashboard
-  # methods pertaining to catalog functionality for dashboard
+  # methods pertaining to catalog (on prem storage) functionality for dashboard
   module CatalogService
     include ActionView::Helpers::NumberHelper # for number_to_human_size
 
