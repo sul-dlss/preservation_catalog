f872e516a9b36794a221656a5897862013d06d5a
Naomi Dushay <ndushay@stanford.edu>
2022-12-02 13:55:42 -0800

    when running a single spec file, run tests in order

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -73,4 +73,16 @@ RSpec.configure do |config|
   #   # end of the spec run, to help surface which specs are running
   #   # particularly slow.
   #   config.profile_examples = 10
+
+  # Run specs in random order to surface order dependencies. If you find an
+  # order dependency and want to debug it, you can fix the order by providing
+  # the seed, which is printed after each run.
+  #     --seed 1234
+  config.order = :random unless config.files_to_run.one?
+
+  # Seed global randomization in this process using the `--seed` CLI option.
+  # Setting this allows you to use `--seed` to deterministically reproduce
+  # test failures related to randomization by passing the same `--seed` value
+  # as the one that triggered the failure.
+  Kernel.srand config.seed
 end
