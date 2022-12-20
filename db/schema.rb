3202fe36e4f2366223835bfddfab3fc2e21e817b
Justin Littman <justinlittman@stanford.edu>
2022-12-20 16:28:36 -0500

    Adds index to ZipParts.
    refs #2084

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2022_12_14_121216) do
+ActiveRecord::Schema[7.0].define(version: 2022_12_20_212633) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -84,6 +84,7 @@ ActiveRecord::Schema[7.0].define(version: 2022_12_14_121216) do
     t.integer "status", default: 1, null: false
     t.datetime "last_existence_check", precision: nil
     t.datetime "last_checksum_validation", precision: nil
+    t.index ["status"], name: "index_zip_parts_on_status"
     t.index ["zipped_moab_version_id", "suffix"], name: "index_zip_parts_on_zipped_moab_version_id_and_suffix", unique: true
     t.index ["zipped_moab_version_id"], name: "index_zip_parts_on_zipped_moab_version_id"
   end
