84e6f4163ae98920c7d592f49185e0b0467266e3
Justin Littman <justinlittman@stanford.edu>
2026-01-12 16:42:36 -0500

    Switch to using Aws transfer manager.
    closes #2525

--- spec/services/replication/replicate_zip_part_service_spec.rb
+++ spec/services/replication/replicate_zip_part_service_spec.rb
@@ -9,18 +9,28 @@ RSpec.describe Replication::ReplicateZipPartService do
   let(:druid_version_zip_part) { instance_double(Replication::DruidVersionZipPart, file_path: '/path/to/file', read_md5: md5, size: 2048) }
   let(:s3_part) { instance_double(Aws::S3::Object) }
   let(:md5) { '00236a2ae558018ed13b5222ef1bd977' }
+  let(:transfer_manager) { instance_double(Aws::S3::TransferManager) }
+  let(:bucket_name) { 'sul-sdr-aws-us-west-2-test' }
+  let(:provider) { instance_double(Replication::AwsProvider, client:, bucket_name:) }
+  let(:client) { instance_double(Aws::S3::Client) }
 
   before do
     allow(zip_part).to receive_messages(s3_part: s3_part, druid_version_zip_part: druid_version_zip_part)
 
     allow(s3_part).to receive(:exists?).and_return(false)
-    allow(s3_part).to receive(:upload_file)
+    allow(Aws::S3::TransferManager).to receive(:new).with(client:).and_return(transfer_manager)
+    allow(transfer_manager).to receive(:upload_file)
+    allow(Replication::ProviderFactory).to receive(:create).and_return(provider)
   end
 
   context 'when the zip part does not exist on the endpoint' do
     it 'uploads the zip part file to the endpoint' do
       subject.call
-      expect(s3_part).to have_received(:upload_file).with('/path/to/file', metadata: { checksum_md5: md5, size: '2048' })
+      expect(transfer_manager).to have_received(:upload_file)
+        .with('/path/to/file',
+              bucket: bucket_name,
+              key: zip_part.s3_key,
+              metadata: { checksum_md5: md5, size: '2048' })
     end
   end
 
@@ -31,7 +41,7 @@ RSpec.describe Replication::ReplicateZipPartService do
 
     it 'does not re-upload the zip part file' do
       subject.call
-      expect(s3_part).not_to have_received(:upload_file)
+      expect(transfer_manager).not_to have_received(:upload_file)
     end
   end
 
@@ -42,7 +52,7 @@ RSpec.describe Replication::ReplicateZipPartService do
 
     it 'raises' do
       expect { subject.call }.to raise_error(Replication::ReplicateZipPartService::DifferentPartFileFoundError)
-      expect(s3_part).not_to have_received(:upload_file)
+      expect(transfer_manager).not_to have_received(:upload_file)
     end
   end
 end
