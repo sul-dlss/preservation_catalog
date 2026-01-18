4c7e306ec818c5f730264d7ff47feaf9ab09c0bc
Justin Littman <justinlittman@stanford.edu>
2026-01-18 16:44:23 -0500

    Remove existing dashboard.
    refs #2538

--- app/components/dashboard/audit_status_component.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-module Dashboard
-  # methods for dashboard pertaining to AuditStatusComponent
-  class AuditStatusComponent < ViewComponent::Base
-    include Dashboard::AuditService
-  end
-end
--- app/components/dashboard/replication_endpoints_component.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-module Dashboard
-  # methods for dashboard pertaining to ReplicationEndpointsComponent
-  class ReplicationEndpointsComponent < ViewComponent::Base
-    include Dashboard::MoabOnStorageService
-    include Dashboard::ReplicationService
-  end
-end
