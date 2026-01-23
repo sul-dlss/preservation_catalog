687e395021500becb7b5bca07a7c6a3978ee2adb
Aaron Collier <aaron.collier@stanford.edu>
2026-01-23 13:45:09 -0800

    Rename stage worker server

--- config/deploy/stage.rb
+++ config/deploy/stage.rb
@@ -2,7 +2,7 @@
 
 server 'preservation-catalog-web-stage-01.stanford.edu', user: 'pres', roles: %w[app web]
 server 'preservation-catalog-web-stage-02.stanford.edu', user: 'pres', roles: %w[app web]
-server 'preservation-catalog-stage-02.stanford.edu', user: 'pres', roles: %w[app db worker queue_populator cache_cleaner]
+server 'preservation-catalog-worker-stage-01.stanford.edu', user: 'pres', roles: %w[app db worker queue_populator cache_cleaner]
 
 set :rails_env, 'production'
 set :bundle_without, 'deploy test'
