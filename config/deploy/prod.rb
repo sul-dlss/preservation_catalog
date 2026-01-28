3a126f5e1140abcc032dea5435e67f51c7da8378
Aaron Collier <aaron.collier@stanford.edu>
2026-01-28 11:39:35 -0800

    Rename prod worker boxes

--- config/deploy/prod.rb
+++ config/deploy/prod.rb
@@ -2,9 +2,9 @@
 
 server 'preservation-catalog-web-prod-01.stanford.edu', user: 'pres', roles: %w[app web]
 server 'preservation-catalog-web-prod-02.stanford.edu', user: 'pres', roles: %w[app web]
-server 'preservation-catalog-prod-02.stanford.edu', user: 'pres', roles: %w[app db worker queue_populator]
-server 'preservation-catalog-prod-03.stanford.edu', user: 'pres', roles: %w[app worker]
-server 'preservation-catalog-prod-04.stanford.edu', user: 'pres', roles: %w[app worker cache_cleaner]
+server 'preservation-catalog-worker-prod-01.stanford.edu', user: 'pres', roles: %w[app db worker queue_populator]
+server 'preservation-catalog-worker-prod-02.stanford.edu', user: 'pres', roles: %w[app worker]
+server 'preservation-catalog-worker-prod-03.stanford.edu', user: 'pres', roles: %w[app worker cache_cleaner]
 
 set :rails_env, 'production'
 set :bundle_without, 'deploy test'
